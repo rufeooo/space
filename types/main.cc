d080aab87cfde03de8cd49976cb53739bb9d01a6
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-05-05 15:42:58 -0700

    Test to compare time difference between setting fields in regular struct vs flatbuffer struct.

--- types/main.cc
+++ types/main.cc
@@ -1,12 +1,55 @@
+#include <chrono> 
 #include <iostream>
+
 #include "common/vec3_generated.h"
 #include "flatbuffers/flatbuffers.h"
 
+struct Vec3 {
+  float x_;
+  float y_;
+  float z_;
+};
+
 int main() {
-  common::Vec3 v;
-  v.mutate_x(3.0f);
-  v.mutate_y(2.3f);
-  v.mutate_z(7.6f);
-  std::cout << v.x() << "," << v.y() << "," << v.z() << std::endl;
+  common::Vec3 vfp;
+  Vec3 v;
+  {
+    // Test speed of setting flatbuffer vs struct
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < 50000; ++i) {
+      vfp.mutate_x(3.0f + float(i));
+      vfp.mutate_y(2.3f + float(i));
+      vfp.mutate_z(7.6f + float(i));
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    auto duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(
+          stop - start); 
+    
+    std::cout << "Flatbuffer: "
+              << duration.count() << " microseconds" << std::endl;
+  }
+  {
+    // Test speed of setting flatbuffer vs struct
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < 50000; ++i) {
+      v.x_ = (3.0f + float(i));
+      v.y_ = (2.3f + float(i));
+      v.z_ = (7.6f + float(i));
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    auto duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(
+          stop - start); 
+    
+    std::cout << "Struct: "
+              << duration.count() << " microseconds" << std::endl;
+  }
   return 0;
 }
