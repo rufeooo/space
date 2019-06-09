9f452f8bbb04dc7573f20cea9b6784659aee8061
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-06-09 14:46:18 -0700

    Whitespace.

--- examples/glfw/main.cc
+++ examples/glfw/main.cc
@@ -36,11 +36,13 @@ static const char* fragment_shader_text =
 static void error_callback(int error, const char* description) {
   fprintf(stderr, "Error: %s\n", description);
 }
+
 static void key_callback(GLFWwindow* window, int key, int scancode,
                          int action, int mods) {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
+
 int main() {
   GLFWwindow* window;
   GLuint vertex_buffer, vertex_shader, fragment_shader, program;
