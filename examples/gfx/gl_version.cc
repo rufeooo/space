c4341d168d1542c712a56ea2b494b53034bee25e
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-06-09 15:52:16 -0700

    Simple gfx example to print GL renderer / version.

---
+++ examples/gfx/gl_version.cc
@@ -0,0 +1,27 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+  if (!glfwInit()) {
+    std::cerr << "Cound not start GLFW3" << std::endl;
+    return 1;
+  }
+  // Kinda doubt I need this.
+  /*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+  GLFWwindow* window = glfwCreateWindow(
+      640, 480, "Hello Triangle", NULL, NULL);
+  glfwMakeContextCurrent(window);
+  gladLoadGL(glfwGetProcAddress);
+  const GLubyte* renderer = glGetString(GL_RENDERER);
+  const GLubyte* version = glGetString(GL_VERSION);
+  std::cout << "Renderer: " << renderer << std::endl;
+  std::cout << "Version: " << version << std::endl;
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+  glfwTerminate();
+  return 0;
+}
