76fd729fd96cf013527cd6f5b5052359e6be5fb2
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-09-08 06:50:37 -0700

    Adds googeltest and an example using it.

---
+++ examples/gtest/main_test.cc
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+
+int Foo(int n) {
+  if (n == 0) return 1;
+  return 0;
+}
+
+TEST(Foo, Negative) {
+  EXPECT_EQ(Foo(-1), 0);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
