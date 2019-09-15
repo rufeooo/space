de14cd2e6cff66924b8eb52e54b16a4c5685c681
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-09-15 10:38:26 -0700

    Example that binds two vbo's to a single vao. The first is a vbo for the verts and the second is for the colors.

--- examples/gfx/gl_colored_triangle.cc
+++ examples/gfx/gl_colored_triangle.cc
@@ -2,19 +2,25 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-const char* vertex_shader =
-  "#version 410\n"
-  "in vec3 vp;"
-  "void main() {"
-  "  gl_Position = vec4(vp, 1.0);"
-  "}";
+const char* vertex_shader = R"(
+  #version 410
+  layout (location = 0) in vec3 vertex_position;
+  layout (location = 1) in vec3 vertex_color;
+  out vec3 color; 
+  void main() {
+    color = vertex_color; 
+    gl_Position = vec4(vertex_position, 1.0);
+  }
+)";
 
-const char* fragment_shader =
-  "#version 410\n"
-	"out vec4 frag_colour;"
-  "void main() {"
-  " frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
-  "}";
+const char* fragment_shader = R"(
+  #version 410
+  in vec3 color;
+	out vec4 frag_color;
+  void main() {
+   frag_color = vec4(color, 1.0);
+  }
+)";
 
 int main() {
   if (!glfwInit()) {
@@ -48,19 +54,37 @@ int main() {
     0.5f, -0.5f, 0.0f,
     -0.5f, -0.5f, 0.0f,
   };
-  // Create VBO.
-  GLuint vbo = 0;
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f,
+  };
+
+  // Create points VBO.
+  GLuint points_vbo = 0;
+  glGenBuffers(1, &points_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
   glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), points,
                GL_STATIC_DRAW);
-  // Create VAO bind vbo to it.
+
+  // Create colors VBO.
+  GLuint colors_vbo = 0;
+  glGenBuffers(1, &colors_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors,
+               GL_STATIC_DRAW);
+
   GLuint vao = 0;
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
+  glGenVertexArrays (1, &vao);
+  glBindVertexArray (vao);
+  // This needs to be done for each bound vao
   glEnableVertexAttribArray(0);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  glEnableVertexAttribArray(1);
+  glBindBuffer (GL_ARRAY_BUFFER, points_vbo);
+  glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  glBindBuffer (GL_ARRAY_BUFFER, colors_vbo);
+  glVertexAttribPointer (1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
   // Create and compile vertex shader.
   GLuint vs = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vs, 1, &vertex_shader, NULL);
@@ -83,14 +107,7 @@ int main() {
     glBindVertexArray(vao);
     //glPolygonMode(GL_FRONT, GL_LINE);
     // Draw them points.
-    // glDrawArrays(ENUM, starting_index, number_indices_to_draw)
-    //glDrawArrays(GL_POINTS, 0, 3); // Can't see anything!
-    //glDrawArrays(GL_LINES, 0, 3); // Only draws one line!
-    //glDrawArrays(GL_LINE_STRIP, 0, 3); // Only draws two lines!
-    //glDrawArrays(GL_LINE_LOOP, 0, 3); // Draws 3 lines making tri!
     glDrawArrays(GL_TRIANGLES, 0, 3); // Draws the triangle
-    //glDrawArrays(GL_TRIANGLE_STRIP, 0, 3); // Draws the triangle
-    //glDrawArrays(GL_TRIANGLE_FAN, 0, 3); // Draws the triangle
     glfwPollEvents();
     glfwSwapBuffers(window);
   }
