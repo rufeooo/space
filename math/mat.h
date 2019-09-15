7235fb6f62cd13ecd93dd04201bdd9d0dd293131
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-09-15 14:03:13 -0700

    Simple matrix implementation.

---
+++ math/mat.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#include <array>
+#include <cassert>
+#include <initializer_list>
+
+namespace math {
+
+// column major matrix
+template <typename T, size_t M, size_t N>
+class Mat {
+ public:
+  virtual ~Mat() = default;
+  Mat() = default;
+  Mat(std::initializer_list<T> l) {
+    assert(l.size() == M * N);
+    int i = 0;
+    for (const auto& v : l) {
+      data_[i++] = v;
+    }
+  }
+
+  T& operator()(size_t m, size_t n) {
+    assert(m * n < M * N);
+    return data_[n * M + m];
+  }
+
+  const T& operator()(size_t m, size_t n) const {
+    assert(m * n < M * N);
+    return data_[n * M + m];
+  }
+
+  size_t rows() const { return M; }
+  size_t cols() const { return N; }
+
+  bool operator==(const Mat& rhs) const {
+    return data_ == rhs.data_;
+  }
+
+  bool operator!=(const Mat& rhs) const {
+    return !(data_ == rhs.data_);
+  }
+
+  std::array<T, M * N> data_;
+};
+
+// This is fun... It's impossible for a program to compile if the
+// multplication is invalid.
+template <typename T, size_t M, size_t N, size_t P>
+Mat<T, M, P> operator*(const Mat<T, M, N>& lhs,
+                       const Mat<T, N, P>& rhs) {
+  Mat<T, M, P> r;
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < P; ++j) {
+      r(i, j) = T(0);
+      for (int k = 0; k < N; ++k) {
+        r(i, j) += lhs(i, k) * rhs(k, j);
+      }
+    }
+  }
+  return r;
+}
+
+template <typename T, size_t M, size_t N>
+Mat<T, M, N> operator*(const Mat<T, M, N>& lhs, const T& rhs) {
+  Mat<T, M, N> r;
+  for (size_t i = 0; i < M * N; ++i) r.data_[i] = lhs.data_[i] * rhs;
+  return r;
+}
+
+template <typename T, size_t M, size_t N>
+Mat<T, M, N> operator*(const T& rhs, const Mat<T, M, N>& lhs) {
+  return lhs * rhs;
+}
+
+
+template <typename T, size_t M, size_t N>
+Mat<T, M, N> operator/(const Mat<T, M, N>& lhs, const T& rhs) {
+  Mat<T, M, N> r;
+  for (size_t i = 0; i < M * N; ++i) r.data_[i] = lhs.data_[i] / rhs;
+  return r;
+}
+
+template <typename T, size_t M, size_t N>
+Mat<T, M, N> operator+(const Mat<T, M, N>& lhs, const T& rhs) {
+  Mat<T, M, N> r;
+  for (size_t i = 0; i < M * N; ++i) r.data_[i] = lhs.data_[i] + rhs;
+  return r;
+}
+
+template <typename T, size_t M, size_t N>
+Mat<T, M, N> operator+(const T& rhs, const Mat<T, M, N>& lhs) {
+  return lhs + rhs;
+}
+
+template <typename T, size_t M, size_t N>
+Mat<T, M, N> operator-(const Mat<T, M, N>& lhs, const T& rhs) {
+  Mat<T, M, N> r;
+  for (size_t i = 0; i < M * N; ++i) r.data_[i] = lhs.data_[i] - rhs;
+  return r;
+}
+
+using Mat4i = Mat<int, 4, 4>;
+using Mat4u = Mat<uint32_t, 4, 4>;
+using Mat4f = Mat<float, 4, 4>;
+using Mat4d = Mat<double, 4, 4>;
+
+}  // math
--- math/mat_test.cc
+++ math/mat_test.cc
@@ -1,5 +1,83 @@
 #include "gtest/gtest.h"
 
+#include "mat.h"
+
+TEST(MatrixTest, MatrixInitialization) {
+  math::Mat4f mat4x4{
+    1.0f, 0.0f, 2.0f, 0.0f,
+    0.0f, 1.0f, 0.0f, 0.0f,
+    3.0f, 0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 1.0f
+  };
+  ASSERT_EQ(mat4x4(0, 2), 3.0f);
+  ASSERT_EQ(mat4x4(2, 0), 2.0f);
+  math::Mat<int, 3, 6> mat3x6 {
+    1, 2, 3,
+    4, 5, 6,
+    7, 8, 9,
+    10, 11, 12,
+    13, 14, 15,
+    16, 17, 18
+  };
+  ASSERT_EQ(mat3x6(0, 0), 1);
+  ASSERT_EQ(mat3x6(1, 0), 2);
+  ASSERT_EQ(mat3x6(2, 0), 3);
+  ASSERT_EQ(mat3x6(0, 3), 10);
+  ASSERT_EQ(mat3x6(1, 3), 11);
+  ASSERT_EQ(mat3x6(2, 3), 12);
+  ASSERT_EQ(mat3x6(0, 5), 16);
+  ASSERT_EQ(mat3x6(1, 5), 17);
+  ASSERT_EQ(mat3x6(2, 5), 18);
+}
+
+TEST(MatrixTest, MatrixMultiplication) {
+  math::Mat<int, 3, 2> mat3x2 {
+    1, 2, 3,
+    4, 5, 6
+  };
+  math::Mat<int, 2, 5> mat2x5 {
+    1, 2,
+    3, 4,
+    5, 6,
+    7, 8,
+    9, 10
+  };
+  math::Mat<int, 3, 5> desired {
+    9, 12, 15,
+    19, 26, 33,
+    29, 40, 51,
+    39, 54, 69,
+    49, 68, 87
+  };
+  auto r = mat3x2 * mat2x5;
+  ASSERT_EQ(r.rows(), 3);
+  ASSERT_EQ(r.cols(), 5);
+  ASSERT_EQ(r, desired);
+  math::Mat<int, 2, 2> mat2x2  { 1, 2, 3, 4 };
+  math::Mat<int, 2, 2> desired2 { 2, 4, 6, 8 };
+  ASSERT_EQ(mat2x2 * 2, desired2);
+  ASSERT_EQ(2 * mat2x2, desired2);
+}
+
+TEST(MatrixTest, MatrixDivision) {
+  math::Mat<int, 2, 2> mat2x2  { 2, 4, 6, 8 };
+  math::Mat<int, 2, 2> desired { 1, 2, 3, 4 };
+  ASSERT_EQ(mat2x2 / 2, desired);
+}
+
+TEST(MatrixTest, MatrixAddition) {
+  math::Mat<int, 2, 2> mat2x2  { 1, 2, 3, 4 };
+  math::Mat<int, 2, 2> desired { 4, 5, 6, 7 };
+  ASSERT_EQ(mat2x2 + 3, desired);
+  ASSERT_EQ(3 + mat2x2, desired);
+}
+
+TEST(MatrixTest, MatrixSubtraction) {
+  math::Mat<int, 2, 2> mat2x2  { 1, 2, 3, 4 };
+  math::Mat<int, 2, 2> desired { -1, 0, 1, 2 };
+  ASSERT_EQ(mat2x2 - 2, desired);
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
