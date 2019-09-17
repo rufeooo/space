48d835a7f2b908ff25c3f12ce31bbb793ed81621
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-09-17 06:33:58 -0700

    Moves CreateTriangleVAO to gl_utils.

--- renderer/gl_utils.cc
+++ renderer/gl_utils.cc
@@ -22,4 +22,25 @@ std::string GLTypeToString(GLenum type) {
   return "other";
 }
 
+uint32_t CreateTriangleVAO() {
+  GLfloat points[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+  };
+  // Create points VBO.
+  GLuint points_vbo = 0;
+  glGenBuffers(1, &points_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), points,
+               GL_STATIC_DRAW);
+  GLuint vao = 0;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  return vao;
+}
+
 }
--- renderer/gl_utils.h
+++ renderer/gl_utils.h
@@ -5,6 +5,15 @@
 
 namespace renderer {
 
+// Given a GLenum returns a stringified version of it. For example -
+// GLTypeToString(GL_BOOL) = "bool"
+// GLTypeToString(GL_INT) = "int"
+// GLTypeToString(GL_FLOAT) = "float"
 std::string GLTypeToString(GLenum type);
+
+// Creates vbo for a triangle and binds a vao with those points.
+// Returns resulting  vao. A user should call those once and cache
+// the id returned from it.
+uint32_t CreateTriangleVAO();
 
 }
