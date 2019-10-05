6e69eb67862f0511c50f2fd29671184a8aff6f3a
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-10-05 10:51:04 -0700

    Gets test working and removed some more references to SDL. I think it's ok to remove the submodule and cmake for it now.

--- game/game_test.cc
+++ game/game_test.cc
@@ -1,12 +1,9 @@
-#define CATCH_CONFIG_MAIN  // Make Catch provide main.
-
 #include <iostream>
-#include <catch2/catch.hpp>
 
+#include "gtest/gtest.h"
 #include "game.h"
-#include "sdl_game.h"
 
-TEST_CASE("Simple Game", "[game]") {
+TEST(Game, HappyPathGameTest) {
   class TestGame : public game::Game {
    public:
     bool ProcessInput() override { return true; }
@@ -16,23 +13,13 @@ TEST_CASE("Simple Game", "[game]") {
   uint64_t count = 500;
   TestGame game;
   game.Run(count);
-  REQUIRE(game.game_updates() == count);
+  ASSERT_EQ(game.game_updates(), count);
   // Total game time should be game updates * milliseconds_per_update.
   // 500 * 15 = 7500
-  REQUIRE(game.game_time() == std::chrono::milliseconds(7500));
+  ASSERT_EQ(game.game_time(), std::chrono::milliseconds(7500));
 }
 
-// TODO: Remove SDL.
-//TEST_CASE("Simple SDL Game", "[sdl-game]") {
-  //class TestSDLGame : public game::SDLGame {
-  // public:
-  //  TestSDLGame() : game::SDLGame(500, 500) {}
-  //  bool Update() override { return false; }
-  //};
-  //TestSDLGame game;
-  //game.Run(10000);
-  // Simply verify a window and renderer were properly created and
-  // initialized. You should see a black screen pop up for this test.
-  //REQUIRE(game.window() != nullptr);
-  //REQUIRE(game.renderer() != nullptr);
-//}
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
