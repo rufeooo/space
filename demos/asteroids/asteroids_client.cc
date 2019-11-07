31ad94fbfc7e1331d61eae25809d86c99b2edbae
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-11-07 15:43:15 -0800

    I didn't like this code...

--- demos/asteroids/asteroids_client.cc
+++ demos/asteroids/asteroids_client.cc
@@ -49,8 +49,6 @@ class AsteroidsClient : public game::Game {
 
   bool ProcessInput() override {
     asteroids::ProcessClientInput(game_options_);
-    SendPlayerState();
-    ReceiveServerState();
     return true;
   }
 
@@ -71,106 +69,6 @@ class AsteroidsClient : public game::Game {
   }
 
  private:
-  void SendPlayerState() {
-    // Singleplayer games don't send data to the server.
-    if (IsSinglePlayer()) return;
-    auto& transform =
-        *game_options_.game_state.components
-            .Get<component::TransformComponent>(player_);
-    flatbuffers::FlatBufferBuilder fbb;
-    auto position = asteroids::Vec3(
-        transform.position.x(), transform.position.y(),
-        transform.position.z());
-    auto orientation = asteroids::Vec4(
-        transform.orientation.x(), transform.orientation.y(),
-        transform.orientation.z(), transform.orientation.w());
-    auto player_transform = asteroids::Transform(
-        position, orientation);
-    auto& physics = *game_options_.game_state.components
-      .Get<asteroids::PhysicsComponent>(player_);
-    auto fb_acceleration = asteroids::Vec3(
-        physics.acceleration.x(), physics.acceleration.y(),
-        physics.acceleration.z());
-    auto fb_velocity = asteroids::Vec3(
-        physics.velocity.x(), physics.velocity.y(),
-        physics.velocity.z());
-    auto player_physics = asteroids::Physics(
-        fb_acceleration, fb_velocity); 
-    auto player_state = asteroids::PlayerState(
-        player_, player_transform, player_physics);
-    auto packet = asteroids::CreatePacket(fbb, &player_state, nullptr);
-    fbb.Finish(packet);
-    outgoing_message_queue_.Enqueue(fbb.Release());
-  }
-
-  void HandlePlayerState(const asteroids::PlayerState* player_state) {
-    if (!player_state) return;
-    uint64_t server_entity = player_state->entity_id();
-    auto client_entity_itr =
-      server_entity_mappings_.find(server_entity);
-  }
-
-  void HandleAsteroidState(
-      const asteroids::AsteroidState* asteroid_state) {
-    if (!asteroid_state) return;
-    auto& components = game_options_.game_state.components;
-    uint64_t server_entity = asteroid_state->entity_id();
-    auto client_entity_itr =
-      server_entity_mappings_.find(server_entity);
-    ecs::Entity client_entity;
-    // Check if the server knows about this client entity.
-    if (client_entity_itr == server_entity_mappings_.end()) {
-      ecs::Entity asteroid_entity = SpawnAsteroid(
-          game_options_, math::Vec3f(0.f, 0.f, 0.f),
-          math::Vec3f(0.f, 0.f, 0.f), 0.f,
-          asteroid_state->random_number());
-      // The server doesn't know  about this entity so make it.
-      client_entity = asteroid_entity;
-      server_entity_mappings_[server_entity] = client_entity;
-    } else { client_entity = client_entity_itr->second; }
-    auto fb_physics = asteroid_state->physics();
-    asteroids::PhysicsComponent* asteroid_physics =
-        components.Get<asteroids::PhysicsComponent>(client_entity);
-    if (!asteroid_physics) return;
-    asteroid_physics->acceleration = math::Vec3f(
-        fb_physics.acceleration().x(),
-        fb_physics.acceleration().y(),
-        fb_physics.acceleration().z());
-    asteroid_physics->velocity = math::Vec3f(
-        fb_physics.velocity().x(),
-        fb_physics.velocity().y(),
-        fb_physics.velocity().z());
-    auto fb_transform = asteroid_state->transform();
-    component::TransformComponent* asteroid_transform =
-        components.Get<component::TransformComponent>(client_entity);
-    if (!asteroid_transform) return;
-    asteroid_transform->position = math::Vec3f(
-        fb_transform.position().x(),
-        fb_transform.position().y(),
-        fb_transform.position().z());
-    //std::cout << "Set: " << server_entity << " position "
-    //          << player_transform->position.String() << std::endl;
-    asteroid_transform->orientation = math::Quatf(
-        fb_transform.orientation().x(),
-        fb_transform.orientation().y(),
-        fb_transform.orientation().z(),
-        fb_transform.orientation().w());
-  }
-
-  void ReceiveServerState() {
-    auto& components = game_options_.game_state.components;
-    network::Message msg = incoming_message_queue_.Dequeue();
-    while (msg.size != 0) {
-      // Deserialize the message into a flatbuffer.
-      const asteroids::Packet* asteroids_packet = asteroids::GetPacket(
-          (const void*)msg.data);
-      HandlePlayerState(asteroids_packet->player_state());
-      HandleAsteroidState(asteroids_packet->asteroid_state());
-      free(msg.data);  // Probably not ideal to do this here.
-      msg = incoming_message_queue_.Dequeue();
-    }
-  }
-
   // Game related.
   asteroids::Options game_options_;
   ecs::Entity player_;
--- demos/asteroids/asteroids_server.cc
+++ demos/asteroids/asteroids_server.cc
@@ -41,13 +41,11 @@ class AsteroidsServer : public game::Game {
 
   bool ProcessInput() override {
     if (!IsHeadless()) glfwPollEvents();
-    ReceivePlayerState();
     return true;
   }
 
   bool Update() override {
     asteroids::UpdateGame(game_options_);
-    SendServerState();
     return true;
   }
 
@@ -58,105 +56,12 @@ class AsteroidsServer : public game::Game {
 
   void OnGameEnd() override {
     if (network_thread_.joinable()) {
-      std::cout << "ON_GAME_END" << std::endl;
       incoming_message_queue_.Stop();
       network_thread_.join();
     }
   }
 
  private:
-  void ReceivePlayerState() {
-    auto& components = game_options_.game_state.components;
-    network::Message msg = incoming_message_queue_.Dequeue();
-    while (msg.size != 0) {
-      // Deserialize the message into a flatbuffer.
-      auto* asteroids_packet = asteroids::GetPacket(
-          (const void*)msg.data);
-      // Get the entity mapping.
-      auto& entity_mapping = client_entity_mappings_[msg.client_id];
-      //std::cout << msg.client_id << std::endl;
-      auto* player_state = asteroids_packet->player_state();
-      uint64_t client_entity = player_state->entity_id();
-      auto server_entity_itr = entity_mapping.find(client_entity);
-      ecs::Entity server_entity;
-      // Check if the server knows about this client entity.
-      if (server_entity_itr == entity_mapping.end()) {
-        // The server doesn't know  about this entity so make it.
-        server_entity = game_options_.free_entity;
-        entity_mapping[client_entity] = server_entity;
-        SpawnPlayer(game_options_, math::Vec3f());
-        std::cout << "Server made player entity: "
-                  << server_entity << std::endl;
-      } else { server_entity = server_entity_itr->second; }
-      // Deserialize data into local entity.
-      auto fb_physics = player_state->physics();
-      asteroids::PhysicsComponent* player_physics =
-          components.Get<asteroids::PhysicsComponent>(server_entity);
-      player_physics->acceleration = math::Vec3f(
-          fb_physics.acceleration().x(),
-          fb_physics.acceleration().y(),
-          fb_physics.acceleration().z());
-      player_physics->velocity = math::Vec3f(
-          fb_physics.velocity().x(),
-          fb_physics.velocity().y(),
-          fb_physics.velocity().z());
-      auto fb_transform = player_state->transform();
-      component::TransformComponent* player_transform =
-          components.Get<component::TransformComponent>(server_entity);
-      player_transform->position = math::Vec3f(
-          fb_transform.position().x(),
-          fb_transform.position().y(),
-          fb_transform.position().z());
-      //std::cout << "Set: " << server_entity << " position "
-      //          << player_transform->position.String() << std::endl;
-      player_transform->orientation = math::Quatf(
-          fb_transform.orientation().x(),
-          fb_transform.orientation().y(),
-          fb_transform.orientation().z(),
-          fb_transform.orientation().w());
-      free(msg.data);  // Probably not ideal to do this here.
-      msg = incoming_message_queue_.Dequeue();
-    }
-  }
-
-  void SendServerState() {
-    for (const auto& asteroid_entity :
-        game_options_.game_state.asteroid_entities) {
-      auto& transform =
-          *game_options_.game_state.components
-              .Get<component::TransformComponent>(asteroid_entity);
-      flatbuffers::FlatBufferBuilder fbb;
-      auto position = asteroids::Vec3(
-          transform.position.x(), transform.position.y(),
-          transform.position.z());
-      auto orientation = asteroids::Vec4(
-          transform.orientation.x(), transform.orientation.y(),
-          transform.orientation.z(), transform.orientation.w());
-      auto asteroid_transform = asteroids::Transform(
-          position, orientation);
-      auto& physics = *game_options_.game_state.components
-          .Get<asteroids::PhysicsComponent>(asteroid_entity);
-      auto fb_acceleration = asteroids::Vec3(
-          physics.acceleration.x(), physics.acceleration.y(),
-          physics.acceleration.z());
-      auto fb_velocity = asteroids::Vec3(
-          physics.velocity.x(), physics.velocity.y(),
-          physics.velocity.z());
-      auto asteroid_physics = asteroids::Physics(
-          fb_acceleration, fb_velocity); 
-      auto& random_number = *game_options_.game_state.components
-          .Get<asteroids::RandomNumberIntChoiceComponent>(
-              asteroid_entity);
-      auto asteroid_state = asteroids::AsteroidState(
-          asteroid_entity, asteroid_transform, asteroid_physics,
-          random_number.random_number);
-      auto packet = asteroids::CreatePacket(
-          fbb, nullptr, &asteroid_state);
-      fbb.Finish(packet);
-      outgoing_message_queue_.Enqueue(fbb.Release());
-    }
-  }
-
   // Game related.
   asteroids::Options game_options_;
 
