6ed0bbcf05f1e637129a81fdc1253d0e0071970f
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-05 08:33:28 -0800

    Removes plog.

--- examples/logging/main.cc
+++ examples/logging/main.cc
@@ -1,11 +1,3 @@
-#include "plog/Log.h"
-
 int main() {
-  plog::init(plog::debug, "logs/Hello.txt");
-
-  PLOGD << "Hello!";
-  PLOG_DEBUG << "Ugly Hello!";
-  PLOG(plog::debug) << "Verbose hello!";
-
   return 0;
 }
