ae628dbce8a9702866828faf5bfc3d2049abab1a
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-07 10:07:10 -0800

    Get out of the habit of using byte offset constants.

--- game/event.cc
+++ game/event.cc
@@ -6,8 +6,8 @@ namespace game {
 Event Decode(uint8_t* msg) {
   Event e;
   memcpy(&e.size, &msg[0], sizeof(e.size));
-  memcpy(&e.metadata, &msg[2], sizeof(e.metadata));
-  e.data = &msg[4];
+  memcpy(&e.metadata, &msg[sizeof(e.size)], sizeof(e.metadata));
+  e.data = &msg[kEventHeaderSize];
   return e;
 }
 
