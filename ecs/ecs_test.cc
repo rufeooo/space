f0fc4e6da52ebf878d3bc87eba356942d7a76d59
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-07 09:30:02 -0800

    Converts ecs_test from Catch2 to googletest.

--- ecs/ecs_test.cc
+++ ecs/ecs_test.cc
@@ -1,7 +1,6 @@
-#define CATCH_CONFIG_MAIN  // Make Catch provide main.
-
 #include <iostream>
-#include <catch2/catch.hpp>
+
+#include "gtest/gtest.h"
 
 #include "ecs.h"
 
@@ -23,7 +22,7 @@ struct VelocityComponent {
 
 struct NullComponent {};
 
-TEST_CASE("Sorted Assign / Get", "[ecs]") {
+TEST(ECS, AssignAndGet) {
   ecs::ComponentStorage<PositionComponent,
                          VelocityComponent,
                          NullComponent> storage;
@@ -34,15 +33,16 @@ TEST_CASE("Sorted Assign / Get", "[ecs]") {
   storage.Assign<PositionComponent>(1, 10, 15);
   storage.Assign<VelocityComponent>(1, 1.0f, 3.0f);
   
-  REQUIRE(storage.Get<PositionComponent>(1)->x_ == 10);
-  REQUIRE(storage.Get<PositionComponent>(1)->y_ == 15);
-  REQUIRE(storage.Get<VelocityComponent>(1)->dx_ == 1.0f);
-  REQUIRE(storage.Get<VelocityComponent>(1)->dy_ == 3.0f);
+  ASSERT_TRUE(storage.Get<PositionComponent>(1)->x_ == 10);
+  ASSERT_TRUE(storage.Get<PositionComponent>(1)->y_ == 15);
+  ASSERT_TRUE(storage.Get<VelocityComponent>(1)->dx_ == 1.0f);
+  ASSERT_TRUE(storage.Get<VelocityComponent>(1)->dy_ == 3.0f);
 
-  REQUIRE(storage.Get<PositionComponent>(15) == nullptr);
-  //REQUIRE(storage.Get<NullComponent>(1) == nullptr);
+  ASSERT_TRUE(storage.Get<PositionComponent>(15) == nullptr);
+  //ASSERT_TRUE(storage.Get<NullComponent>(1) == nullptr);
 }
-TEST_CASE("Unsorted Assign / Get", "[ecs]") {
+
+TEST(ECS, AssignAndGetTwo) {
   ecs::ComponentStorage<PositionComponent, VelocityComponent> storage;
   storage.Assign<PositionComponent>(3, 1, 2);
   storage.Assign<VelocityComponent>(3, 1.0f, 3.0f);
@@ -55,13 +55,13 @@ TEST_CASE("Unsorted Assign / Get", "[ecs]") {
   storage.Assign<PositionComponent>(4, 3, 4);
   storage.Assign<VelocityComponent>(4, 1.0f, 3.0f);
 
-  REQUIRE(storage.Get<VelocityComponent>(2)->dx_ == 15.0f);
-  REQUIRE(storage.Get<PositionComponent>(4)->x_ == 3);
-  REQUIRE(storage.Get<VelocityComponent>(3)->dy_ == 3.0f);
-  REQUIRE(storage.Get<PositionComponent>(1)->y_ == 7);
+  ASSERT_TRUE(storage.Get<VelocityComponent>(2)->dx_ == 15.0f);
+  ASSERT_TRUE(storage.Get<PositionComponent>(4)->x_ == 3);
+  ASSERT_TRUE(storage.Get<VelocityComponent>(3)->dy_ == 3.0f);
+  ASSERT_TRUE(storage.Get<PositionComponent>(1)->y_ == 7);
 }
 
-TEST_CASE("Removed", "[ecs]") {
+TEST(ECS, EnumerateAfterRemoval) {
   ecs::ComponentStorage<PositionComponent, VelocityComponent> storage;
   storage.Assign<PositionComponent>(3, 1, 2);
   storage.Assign<VelocityComponent>(3, 1.0f, 3.0f);
@@ -73,26 +73,24 @@ TEST_CASE("Removed", "[ecs]") {
   storage.Assign<VelocityComponent>(7, 5.0f, 0.0f);
   storage.Assign<PositionComponent>(4, 3, 4);
   storage.Assign<VelocityComponent>(4, 1.0f, 3.0f);
-  REQUIRE(storage.Get<VelocityComponent>(2) != nullptr);
+  ASSERT_TRUE(storage.Get<VelocityComponent>(2) != nullptr);
   storage.Remove<VelocityComponent>(2);
-  REQUIRE(storage.Get<VelocityComponent>(2) == nullptr);
+  ASSERT_TRUE(storage.Get<VelocityComponent>(2) == nullptr);
   int i = 0;
   storage.Enumerate<PositionComponent, VelocityComponent>(
       [&](ecs::Entity ent, PositionComponent&, VelocityComponent&) {
-    REQUIRE(ent != 2);
+    ASSERT_TRUE(ent != 2);
     ++i;
   }); 
-  REQUIRE(i == 4);
+  ASSERT_TRUE(i == 4);
 }
 
-TEST_CASE("Sorted insertion and Enumerate", "[ecs]") {
+TEST(ECS, VaryingDensityEnumeration) {
   struct Foo {};
   struct Bar {};
   struct Baz {};
 
-  // This Section is to verify the example in the comments of ecs.h
-  // works correctly.
-  SECTION("Comments section enumeration test.") {
+  {
     ecs::ComponentStorage<Foo, Bar, Baz> storage;
     // Entity 1 has all the components.
     storage.Assign<Foo>(1);
@@ -111,27 +109,27 @@ TEST_CASE("Sorted insertion and Enumerate", "[ecs]") {
     storage.Enumerate<Foo>([&](ecs::Entity e, Foo& /*foo*/) {
       entities.push_back(e);
     });
-    REQUIRE(entities == std::vector<ecs::Entity>({1, 2, 3, 4}));
+    ASSERT_TRUE(entities == std::vector<ecs::Entity>({1, 2, 3, 4}));
     entities.clear();
     storage.Enumerate<Foo, Bar>([&](
         ecs::Entity e, Foo& /*foo*/, Bar& /*bar*/) {
       entities.push_back(e);
     });
-    REQUIRE(entities == std::vector<ecs::Entity>({1, 3}));
+    ASSERT_TRUE(entities == std::vector<ecs::Entity>({1, 3}));
     entities.clear();
     storage.Enumerate<Foo, Baz>([&](ecs::Entity e, Foo& foo, Baz& baz) {
       entities.push_back(e);
     });
-    REQUIRE(entities == std::vector<ecs::Entity>({1, 4}));
+    ASSERT_TRUE(entities == std::vector<ecs::Entity>({1, 4}));
     entities.clear();
     storage.Enumerate<Foo, Bar, Baz>([&](
         ecs::Entity e, Foo& foo, Bar& bar, Baz& baz) {
       entities.push_back(e);
     });
-    REQUIRE(entities == std::vector<ecs::Entity>({1}));
+    ASSERT_TRUE(entities == std::vector<ecs::Entity>({1}));
   }
 
-  SECTION("Large scale test.") {
+  {
     ecs::ComponentStorage<Foo, Bar, Baz> storage;
     for (int i = 1; i < 15000; ++i) storage.Assign<Foo>(i);
     for (int i = 400; i < 2000; ++i) storage.Assign<Bar>(i);
@@ -139,10 +137,10 @@ TEST_CASE("Sorted insertion and Enumerate", "[ecs]") {
     storage.Enumerate<Foo, Bar>([&](ecs::Entity e, Foo& foo, Bar& bar) {
       ++i;
     });
-    REQUIRE(i == 1600);
+    ASSERT_TRUE(i == 1600);
   }
 
-  SECTION("Sparse intersection.") {
+  {
     ecs::ComponentStorage<Foo, Bar, Baz> storage;
     std::vector<ecs::Entity> sparse_list(
         {3, 76, 133, 223, 4567, 33456});
@@ -152,10 +150,10 @@ TEST_CASE("Sorted insertion and Enumerate", "[ecs]") {
     storage.Enumerate<Foo, Bar>([&](ecs::Entity e, Foo& foo, Bar& bar) {
       intersection_list.push_back(e);
     });
-    REQUIRE(sparse_list == intersection_list);
+    ASSERT_TRUE(sparse_list == intersection_list);
   }
 
-  SECTION("Dense intersection.") {
+  {
     ecs::ComponentStorage<Foo, Bar, Baz> storage;
     for (int i = 1; i < 10000; ++i) storage.Assign<Foo>(i);
     for (int i = 1; i < 10000; ++i) storage.Assign<Bar>(i);
@@ -163,14 +161,14 @@ TEST_CASE("Sorted insertion and Enumerate", "[ecs]") {
     int i = 1;
     storage.Enumerate<Foo, Bar, Baz>([&](
         ecs::Entity e, Foo& foo, Bar& bar, Baz& baz) {
-      REQUIRE(e == i);
+      ASSERT_TRUE(e == i);
       ++i;
     });
-    REQUIRE(i == 10000);
+    ASSERT_TRUE(i == 10000);
   }
 }
 
-TEST_CASE("Unsorted insertion and Enumerate", "[ecs]") {
+TEST(ECS, SimpleEnumeration) {
   ecs::ComponentStorage<PositionComponent, VelocityComponent> storage;
   storage.Assign<PositionComponent>(3, 1, 2);
   storage.Assign<PositionComponent>(1, 5, 7);
@@ -189,11 +187,11 @@ TEST_CASE("Unsorted insertion and Enumerate", "[ecs]") {
        VelocityComponent& /*velocity*/) {
     entities.push_back(e);
   });
-  REQUIRE(entities ==
+  ASSERT_TRUE(entities ==
       std::vector<ecs::Entity>({1, 2, 4, 7}));
 }
 
-TEST_CASE("Mutating components during Enumerate", "[ecs]") {
+TEST(ECS, MutationDuringEnumeration) {
   struct Component {
     Component() = default;
     Component(int n) : n_(n) {}; int n_;
@@ -210,11 +208,11 @@ TEST_CASE("Mutating components during Enumerate", "[ecs]") {
   // Assert the above assumption is true (1 through 11).
   int i = 1;
   storage.Enumerate<Component>([&](ecs::Entity /*e*/, Component& comp) {
-    REQUIRE(comp.n_ == i++);
+    ASSERT_TRUE(comp.n_ == i++);
   });
 }
 
-TEST_CASE("Delete Entity", "[ecs]") {
+TEST(ECS, DeleteEntity) {
   ecs::ComponentStorage<PositionComponent, VelocityComponent> storage;
   storage.Assign<PositionComponent>(3, 1, 2);
   storage.Assign<PositionComponent>(1, 5, 7);
@@ -234,6 +232,11 @@ TEST_CASE("Delete Entity", "[ecs]") {
        VelocityComponent& /*velocity*/) {
     entities.push_back(e);
   });
-  REQUIRE(entities ==
+  ASSERT_TRUE(entities ==
       std::vector<ecs::Entity>({1, 4, 7}));
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
