e35beb725cc695060dd0aec02d7bedc9152c4026
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-12 18:42:11 -0800

    Static -> constexpr in header.

--- demos/asteroids/asteroids.h
+++ demos/asteroids/asteroids.h
@@ -17,17 +17,17 @@ namespace asteroids {
 
 //////// Ship Constants //////// 
 // Seconds until the ship reaches max speed.
-static float kSecsToMaxSpeed = 3.f;
+constexpr float kSecsToMaxSpeed = 3.f;
 // TODO: Convert this to seconds until velocity dampens to 0.
-static float kDampenVelocity = 0.00001f;
+constexpr float kDampenVelocity = 0.00001f;
 // How fast the ship accelerates.
-static float kShipAcceleration = 0.00004f;
+constexpr float kShipAcceleration = 0.00004f;
 // Calculation for seconds for the ship to fully rotate.
 //     15 ms an update (see game.h)
 //     ms till full speed = kSecsToFullRotation * 1000.f
 //     rotation degrees a game loop = ms till full speed / 360.f
-static float kSecsToFullRotation = 1.5f;
-static float kRotationSpeed =
+constexpr float kSecsToFullRotation = 1.5f;
+constexpr float kRotationSpeed =
     (1000.f * kSecsToFullRotation) / 360.f;
 
 //////// Projectile Constants //////// 
@@ -36,13 +36,13 @@ static float kRotationSpeed =
 //     (n * 1000) / 15
 //     (4 * 1000) / 15 = 266.6 = to int -> 266
 // TODO: Convert this to seconds for projectile to live.
-static int kProjectileUpdatesToLive = 266;
+constexpr int kProjectileUpdatesToLive = 266;
 // Initial / max speed of projectile.
-static float kProjectileSpeed = 0.005f;
+constexpr float kProjectileSpeed = 0.005f;
 
 //////// Game Constants //////// 
-static float kSecsToSpawnAsteroid = 3.f;
-static uint64_t kMaxAsteroidCount = 100;
+constexpr float kSecsToSpawnAsteroid = 3.f;
+constexpr uint64_t kMaxAsteroidCount = 100;
 
 bool Initialize();
 
