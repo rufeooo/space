1d3f6a7d211df2f0ca775783066c7d9d9d78de81
Alan Newton <rufeooo@gmail.com>
2019-12-19 14:22:08 -0800

    Automate discovery of tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -2,42 +2,16 @@
 
 source run_constants.sh
 
+REPO_TESTS=(`find src -type f -iname "*_test.cc"`)
+
+pushd $BUILD_DIR
 # Manually make all test targets and run them.
 # TODO: Is there a more clever way to find all test targets? Maybe
 # cmake can intelligently make a target for all test files.
-make ecs_test
-./src/ecs_test
-
-make vec_test
-./src/vec_test
-
-make mat_test
-./src/mat_test
-
-make quat_test
-./src/quat_test
-
-make mat_ops_test
-./src/mat_ops_test
-
-#make server_test
-#./src/server_test
-
-#make client_test
-#./src/client_test
-
-make intersection_test
-./src/intersection_test
-
-make game_test
-./src/game_test
-
-make event_buffer_test
-./src/event_buffer_test
-
-#make gl_shader_cache_test
-#./src/gl_shader_cache_test
-
-#make search_test
-#./src/search_test
-
+for test in ${REPO_TESTS[@]}; do
+  echo make $test
+  make `basename -s .cc $test`
+  # or maybe this?
+  # clang++ -std=c++17 -I third_party/googletest/googletest/include/ src/math/mat_ops_test.cc -L build/lib/ -lgtest -lpthread
+done
+popd
--- run_constants.sh
+++ run_constants.sh
@@ -8,14 +8,11 @@ LOG_DIR="logs"
 # is here in the batch script. The logging library we are using, plog,
 # cannot create dirs itself so maybe the solution here is to modify it
 # such that it does.
-mkdir $LOG_DIR
+mkdir $LOG_DIR -p
 
 # Make the build directory if it doesn't already exist.
-if [[ ! -e $BUILD_DIR ]]; then
-  mkdir $BUILD_DIR
-  cd $BUILD_DIR
-  cmake ../
-else
-  # This assumes if the build directory already exists cmake was run.
-  cd $BUILD_DIR
-fi
+mkdir $BUILD_DIR -p
+
+pushd $BUILD_DIR
+cmake ../
+popd
