9c3cbf2d5fce0ea29466c4f94a3174ed43cc5387
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-20 15:04:53 -0800

    Example now creates a modern opengl context.

--- example/windows/build.bat
+++ example/windows/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 REM If VisualStudioVersion not defined run vcvarsall
-IF NOT defined VisualStudioVersion call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+IF NOT DEFINED VisualStudioVersion call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-cl win32_main.cc /nologo /DUNICODE /link user32.lib
+cl win32_main.cc /std:c++latest /nologo /DUNICODE /link user32.lib opengl32.lib gdi32.lib
