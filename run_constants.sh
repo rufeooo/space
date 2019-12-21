12b2f8b1d13544b0a4604c0bc32cc2caa770ee6e
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-20 16:15:29 -0800

    Removes unused build stuff.

--- run_constants.sh
+++ run_constants.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-BUILD_DIR="build"
-LOG_DIR="logs"
 BIN_DIR="bin"
 
-# Not a great situation but this assumes logs will go into the root
-# of the project and binaries will be run from the same location.
-# TODO: Resolve paths for logs - the running c++ binary needs to know
-# where the logs should go and that should correspond with whatever
-# is here in the batch script. The logging library we are using, plog,
-# cannot create dirs itself so maybe the solution here is to modify it
-# such that it does.
-mkdir $LOG_DIR -p
-
-# Make the build directory if it doesn't already exist.
-mkdir $BUILD_DIR -p
 # Make the bin directory if it doesn't already exist.
 mkdir $BIN_DIR -p
 
