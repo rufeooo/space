9ebb25d987bb38e7419c2a865b683646ee8dd92e
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-20 16:22:06 -0800

    More logical ordering of command line arguments.

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ call run_constants.bat
 set file_arg=%1
 for /F "delims=" %%i in ("%file_arg%") do set filename="%%~ni"
 
-CL /Fo:%BIN_DIR%\ /std:c++latest %1 /nologo /DUNICODE /link user32.lib opengl32.lib gdi32.lib /OUT:%BIN_DIR%/%filename%.exe
+CL %1 /Fo:%BIN_DIR%\ /std:c++latest /nologo /DUNICODE /link user32.lib opengl32.lib gdi32.lib /OUT:%BIN_DIR%/%filename%.exe
