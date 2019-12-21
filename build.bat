937003dbf57d486370d871a33e4e753571a32855
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-20 16:46:54 -0800

    Quotes.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 call run_constants.bat
-
+@echo on
 set file_arg=%1
 for /F "delims=" %%i in ("%file_arg%") do set filename="%%~ni"
 
-CL %1 /nologo /std:c++17 /I "src\" /Fo:%BIN_DIR%\ /DUNICODE /link user32.lib opengl32.lib gdi32.lib /OUT:%BIN_DIR%/%filename%.exe
+cl %1 /nologo /std:c++latest /I src\ /Fo:%BIN_DIR%\ /DUNICODE /link user32.lib opengl32.lib gdi32.lib /OUT:%BIN_DIR%/%filename%.exe
