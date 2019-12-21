15357497018e732f231b8c349fd6719569709cc8
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-20 18:14:06 -0800

    Guess I should be consistent with batch casing.

--- run_constants.bat
+++ run_constants.bat
@@ -1,5 +1,5 @@
 @echo off
-SET BIN_DIR="bin"
+set BIN_DIR="bin"
 
-IF NOT DEFINED VisualStudioVersion call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-IF NOT EXIST "%BIN_DIR%" mkdir "%BIN_DIR%"
+if not defined VisualStudioVersion call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+if not exist "%BIN_DIR%" mkdir "%BIN_DIR%"
