6c8604559a7a85f2b8f8921256c7b47c83691bea
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-20 16:14:40 -0800

    Windows version of build scripts.

---
+++ build.bat
@@ -0,0 +1,6 @@
+call run_constants.bat
+
+set file_arg=%1
+for /F "delims=" %%i in ("%file_arg%") do set filename="%%~ni"
+
+CL /Fo:%BIN_DIR%\ /std:c++latest %1 /nologo /DUNICODE /link user32.lib opengl32.lib gdi32.lib /OUT:%BIN_DIR%/%filename%.exe
---
+++ run_constants.bat
@@ -0,0 +1,4 @@
+SET BIN_DIR="bin"
+
+IF NOT DEFINED VisualStudioVersion call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+IF NOT EXIST "%BIN_DIR%" mkdir "%BIN_DIR%"
