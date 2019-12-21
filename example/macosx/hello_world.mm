b4383362eecad81b09c8932fdde77b85914e8f41
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-21 14:25:55 -0800

    ObjC hello world.

---
+++ example/macosx/hello_world.mm
@@ -0,0 +1,17 @@
+// NSArray, NSDictionary, NSSet...
+#include <Foundation/Foundation.h>
+
+int main (int argc, const char** argv) {
+  // Supports reference-counted memory management.
+  // Even though I may not want to use it cocoa library uses it extensively
+  // so I need to prepare the pool. Autoreleased objects will leak if I do
+  // not do this.
+  // https://developer.apple.com/documentation/foundation/nsautoreleasepool
+  // Deeper explanation
+  // https://www.mikeash.com/pyblog/friday-qa-2011-09-02-lets-build-nsautoreleasepool.html
+  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+
+  NSLog(@"Hello world");
+  [pool drain];
+  return 0;
+}
