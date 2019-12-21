ad7c93b287c4ce5f0d71bdfde1d0970ba16b75df
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-20 16:36:17 -0800

    Adds include directory.

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
-@echo off
 call run_constants.bat
 
 set file_arg=%1
 for /F "delims=" %%i in ("%file_arg%") do set filename="%%~ni"
 
-CL %1 /Fo:%BIN_DIR%\ /std:c++latest /nologo /DUNICODE /link user32.lib opengl32.lib gdi32.lib /OUT:%BIN_DIR%/%filename%.exe
+CL %1 /nologo /std:c++17 /I "src\" /Fo:%BIN_DIR%\ /DUNICODE /link user32.lib opengl32.lib gdi32.lib /OUT:%BIN_DIR%/%filename%.exe
