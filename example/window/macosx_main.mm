cd32ebea55c6714d54fab2e549b1876fa1fd551d
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-22 10:04:24 -0800

    Macosx platform window layer.

--- example/window/macosx_main.mm
+++ example/window/macosx_main.mm
@@ -1,125 +1,19 @@
-#include <OpenGL/gl3.h>
 #include <iostream>
-
-#import <Cocoa/Cocoa.h>
-#import <memory>
-
-
-NSApplication* application;
-
-static bool kTerminate = false;
-
-@interface MacApp : NSWindow <NSApplicationDelegate>{
-}
-
-@property (nonatomic, retain) NSOpenGLView* glView;
-
-@end
-
-@implementation MacApp
-
--
-(id)
-initWithContentRect:(NSRect)contentRect
-                     styleMask:(NSUInteger)aStyle
-                     backing:(NSBackingStoreType)bufferingType
-                     defer:(BOOL)flag
-{
-  if(self = [super initWithContentRect:contentRect
-                   styleMask:aStyle
-                   backing:bufferingType
-                   defer:flag]) {
-    [self setTitle:[[NSProcessInfo processInfo] processName]];
-
-    // This initializes GL to 4.1 WHY.
-    NSOpenGLPixelFormatAttribute pixelFormatAttributes[] ={
-        NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core,
-            NSOpenGLPFAColorSize, 24,
-            NSOpenGLPFAAlphaSize, 8,
-            NSOpenGLPFADoubleBuffer,
-            NSOpenGLPFAAccelerated,
-            NSOpenGLPFANoRecovery,
-            0
-    };
-
-    NSOpenGLPixelFormat* format =
-        [[NSOpenGLPixelFormat alloc]initWithAttributes:pixelFormatAttributes];
-    _glView =
-        [[NSOpenGLView alloc]initWithFrame:contentRect pixelFormat:format];
-    [[_glView openGLContext]makeCurrentContext];
-  
-    [self setContentView:_glView];
-    [_glView prepareOpenGL];
-    [self makeKeyAndOrderFront:self];
-    [self setAcceptsMouseMovedEvents:YES];
-    [self makeKeyWindow];
-    [self setOpaque:YES];
-  }
-  return self;
-}
-
-- (BOOL)
-applicationShouldTerminateAfterLastWindowClosed:(NSApplication*)theApplication
-{
-  std::cout << "applicationShouldTerminateAfterLastWindowClosed" << std::endl;
-  kTerminate = true;
-  return YES;
-}
-
-@end
-
-//@synthesize glView;
+#include "platform/macosx_window.mm"
+#include "math/vec.h"
 
 int
-main(int argc, const char * argv[])
+main(int argc, char** argv)
 {
-  MacApp* app;
-  application = [NSApplication sharedApplication];
-  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular]; 
-  //create a window with the size of 600 by 600   
-  app = [[MacApp alloc] initWithContentRect:NSMakeRect(0, 0, 600, 600)
-         styleMask:NSTitledWindowMask | NSClosableWindowMask |  NSMiniaturizableWindowMask
-         backing:NSBackingStoreBuffered defer:YES];    
-  [application setDelegate:app];
+  window::Create("macosx is great", 1024, 768);
+  while (!window::ShouldClose()) {
+    window::PollEvents();
 
-  // Not really sure why I need this or if I need this.
-  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-  while (!kTerminate) {
-    NSEvent* event;
-    while ((event =  [NSApp nextEventMatchingMask:NSEventMaskAny
-                                        untilDate:[NSDate distantPast]
-                                           inMode:NSDefaultRunLoopMode
-                                          dequeue:YES])) {
-      NSEventType event_type = [event type];
-      switch (event_type) {
-        case NSLeftMouseDown: {
-          // TODO: How to get mouse position?
-          NSLog(@"Left mouse down");
-        } break;
-        case NSKeyDown: {
-          NSString* characters; 
-          characters = [event charactersIgnoringModifiers];
-          unsigned int characterIndex, characterCount;
-          characterCount = [characters length]; 
-          for (characterIndex = 0; characterIndex < characterCount; characterIndex++) {
-            unichar c = [characters characterAtIndex:characterIndex];
-            std::cout << c << std::endl;
-          }
-        } break;
-        default:
-          break;
-      }
-      // Send event to application so the user can do things like close the window.
-      [application sendEvent:event];
-    }
+    glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    //glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
-    //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    [application updateWindows];
+    window::SwapBuffers(); 
   }
-
-  [pool release];
 
   return 0;
 }
---
+++ src/platform/macosx_window.mm
@@ -0,0 +1,193 @@
+#include "window.h"
+
+#include "math/vec.h"
+
+#import <Cocoa/Cocoa.h>
+#import <OpenGL/OpenGL.h>
+#import <OpenGL/gl.h>
+
+@interface OpenGLView : NSView {
+@private
+  NSOpenGLContext* gl_context_;
+}
+
+  //@property (readonly, retain) NSOpenGLContext* gl_context;
+- (id) init;
+- (id) initWithFrame:(NSRect)rect glContext:(NSOpenGLContext*)ctx;
+- (BOOL) isOpaque;
+- (BOOL) canBecomeKeyView;
+- (BOOL) acceptsFirstResponder;
+@end
+
+@implementation OpenGLView
+- (id)
+init
+{
+  return nil;
+}
+
+- (id)
+initWithFrame:(NSRect)rect glContext:(NSOpenGLContext*)ctx
+{
+  self = [super initWithFrame:rect];
+  if (self == nil)
+    return nil;
+  gl_context_ = ctx;
+  return self;
+}
+
+- (BOOL)
+isOpaque
+{
+  return YES;
+}
+
+- (BOOL)
+canBecomeKeyView
+{
+  return YES;
+}
+
+- (BOOL)
+acceptsFirstResponder
+{
+  return YES;
+}
+@end
+
+@interface BaseWindow : NSWindow
+- (void) close;
+
+- (BOOL) acceptsFirstResponder;
+@end
+
+@implementation BaseWindow
+// Close the window - https://developer.apple.com/documentation/appkit/nswindow/1419662-close?language=objc
+// This also terminates the NSApplication 
+- (void)
+close
+{
+  [NSApp terminate:self];
+  // If the app refused to terminate, this window should still close.
+  [super close];
+}
+
+// The receiver is the first object in the responder chain to be sent
+// key events and action messages.
+// https://developer.apple.com/documentation/appkit/nsresponder/1528708-acceptsfirstresponder?language=objc
+- (BOOL)
+acceptsFirstResponder
+{
+  return YES;
+}
+@end
+
+namespace window
+{
+
+struct Window {
+  // TODO: Verify this is needed. Likely doesn't matter though since
+  // few allocations should be going on in here.
+  NSAutoreleasePool* nspool;
+
+  NSView* nsview;
+
+  NSWindow* nswindow;
+
+  NSOpenGLContext* gl_context;
+};
+
+static Window kWindow;
+
+NSOpenGLContext*
+CreateOpenGLContext()
+{
+  CGLError error;
+  CGLPixelFormatAttribute pixel_attrs[] = {
+      kCGLPFADoubleBuffer,
+      kCGLPFAOpenGLProfile, (CGLPixelFormatAttribute)kCGLOGLPVersion_3_2_Core,
+      kCGLPFAColorSize, (CGLPixelFormatAttribute)24,
+      kCGLPFAAlphaSize, (CGLPixelFormatAttribute)8,
+      kCGLPFADepthSize, (CGLPixelFormatAttribute)24,
+      kCGLPFAStencilSize, (CGLPixelFormatAttribute)8,
+      kCGLPFASampleBuffers, (CGLPixelFormatAttribute)0,
+      (CGLPixelFormatAttribute) 0,
+  };
+
+  int ignore;
+  CGLPixelFormatObj pixel_format;
+  error = CGLChoosePixelFormat(pixel_attrs, &pixel_format, &ignore);
+  assert(!error);
+  assert(pixel_format);
+
+  // Create the GL context.
+  CGLContextObj context;
+  error = CGLCreateContext(pixel_format, 0, &context);
+  assert(!error);
+  assert(context);
+
+  return [[NSOpenGLContext alloc] initWithCGLContextObj:context];
+}
+
+
+void Create(const char* name, int width, int height) {
+  kWindow.gl_context = CreateOpenGLContext();
+
+  unsigned int styleMask = NSTitledWindowMask | NSClosableWindowMask;
+  kWindow.nsview = [[OpenGLView alloc]
+                  initWithFrame:NSMakeRect(0,0, width, height)
+                      glContext:kWindow.gl_context]; // TODO: Remove context.
+
+  kWindow.nswindow = [[BaseWindow alloc]
+                      initWithContentRect:[kWindow.nsview frame]
+                                styleMask:styleMask
+                                  backing:NSBackingStoreBuffered
+                                    defer:NO];
+
+  [kWindow.nswindow autorelease];
+  [kWindow.nswindow setTitle:@"Title"];
+  [kWindow.nswindow setContentView:kWindow.nsview];
+  [kWindow.nswindow makeFirstResponder:kWindow.nsview];
+  [kWindow.nswindow center];
+  [kWindow.nswindow makeKeyAndOrderFront:nil];
+  [kWindow.nswindow setAcceptsMouseMovedEvents:YES];
+
+  [kWindow.gl_context makeCurrentContext];
+  [kWindow.gl_context setView:kWindow.nsview];
+}
+
+void PollEvents() {
+  NSEvent* event;
+  // Drain event loop.
+  while ((event =  [NSApp nextEventMatchingMask:NSEventMaskAny
+                                      untilDate:[NSDate distantPast]
+                                         inMode:NSDefaultRunLoopMode
+                                        dequeue:YES])) {
+    [NSApp sendEvent:event];
+  }
+}
+
+void SwapBuffers() {
+  [kWindow.gl_context flushBuffer];
+}
+
+bool ShouldClose() {
+  return false;
+}
+
+math::Vec2f GetWindowSize() {
+  // TODO: I have a feeling this is not the right size.
+  NSSize size = [kWindow.nswindow minSize];
+  return math::Vec2f(size.width, size.height);
+}
+
+math::Vec2f GetCursorPosition() {
+  NSPoint pos;
+  pos = [kWindow.nswindow mouseLocationOutsideOfEventStream];
+  return math::Vec2f(pos.x, pos.y);
+}
+
+bool HasInput(Input input) {
+  return false;
+}
+}  // namespace window
