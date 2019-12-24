88748f568f2aa69f756dfe07d4609cb1562600c0
Alan Newton <rufeooo@gmail.com>
2019-12-24 15:44:38 -0800

    Time merge

--- src/platform/platform.cc
+++ src/platform/platform.cc
@@ -6,15 +6,14 @@
 #endif
 
 #include "platform_getopt.cc"
+#include "platform_clock.cc"
 #include "window.cc"
 
 #if _WIN32
-#include "win32_clock.cc"
 #include "win32_filesystem.cc"
 #include "win32_udp.cc"
 #include "win32_sleep.cc"
 #else
-#include "unix_clock.cc"
 #include "unix_filesystem.cc"
 #include "unix_udp.cc"
 #include "unix_sleep.cc"
