2e71038bbb42796f0a5173888f2ab2a7c47b9cba
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-24 10:18:14 -0800

    Makes win32_clock / GetSystemTimeAsFileTime call functionally equiavelent (with less precision) to unix_clock / clock_gettime.

--- src/platform/win32_clock.cc
+++ src/platform/win32_clock.cc
@@ -5,9 +5,13 @@ namespace platform
 uint64_t
 now_ns()
 {
-  FILETIME time;
-  GetSystemTimeAsFileTime(&time);
-  // TODO: Not sure this is correct. Windows behaves quicker than I'd expect it to.
-  return (((uint64_t)(time.dwHighDateTime) << 32) | (uint64_t)time.dwLowDateTime) * 1000;
+  __int64 wintime;
+  // Gives 100ns precision.
+  GetSystemTimeAsFileTime((FILETIME*)&wintime);
+  // Convert jan 1 1601 to jan 1 1970.
+  wintime -= 116444736000000000i64;
+  // Get windows seconds, convert it to nanoseconds and add nanoseconds.
+  // This makes this quall functionally equiavlent to clock_gettime on unix. See unix_clock.cc
+  return (wintime / 10000000i64) * 1000000000 + (wintime % 10000000i64 * 100);
 }
 }  // namespace platform
