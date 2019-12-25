e1661eb010ef5ce49e87c69dd505ef81da9d4f35
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-25 09:23:40 -0800

    rdtsc on windows.

--- src/platform/rdtsc.h
+++ src/platform/rdtsc.h
@@ -4,6 +4,9 @@
 
 #if defined(__i386__) || defined(__x86_64__)
 #define rdtsc() __builtin_ia32_rdtsc()
+#elif _WIN32
+#include <intrin.h>
+#define rdtsc() __rdtsc();
 #else
 #error "rdtsc() undefined for hardware"
 #endif
