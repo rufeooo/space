ae113e03df07714666c8f17ce5d96116a2f6c0d0
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2019-12-26 16:43:37 -0800

    Refactor loader into its own function.

--- example/gfx/tga_loader.cc
+++ example/gfx/tga_loader.cc
@@ -24,12 +24,14 @@ struct TgaHeader {
 };
 #pragma pack(pop)
 
-int main(int argc, char** argv) {
+uint32_t
+LoadTGA(const char* file, uint8_t** image_bytes)
+{
   FILE* fptr;
   uint8_t* buffer;
   uint32_t file_length;
 
-  fptr = fopen("example/gfx/characters_0.tga", "rb");
+  fptr = fopen(file, "rb");
   fseek(fptr, 0, SEEK_END);
   file_length = ftell(fptr);
   rewind(fptr);
@@ -38,45 +40,45 @@ int main(int argc, char** argv) {
 
   // First load the header.
   TgaHeader* header = (TgaHeader*)buffer;
-  printf("File size: %i\n", file_length);
-  printf("id_length: %hhx\n", header->id_length);
-  printf("color_map_type: %x\n", header->color_map_type);
-  printf("image_type: %x\n", header->image_type);
-  printf("color_map_spec: %.*s\n", 5, header->color_map_spec); // probably 0 if no color
-
-  printf("%zi\n", sizeof(TgaHeader));
-  // Get the image_spec. This has overall image details. 
-  TgaImageSpec* image_spec = (TgaImageSpec*)(&buffer[sizeof(TgaHeader)]);
-  printf("x_origin: %i\n", image_spec->x_origin);
-  printf("y_origin: %i\n", image_spec->y_origin);
-  printf("image_width: %i\n", image_spec->image_width);
-  printf("image_height: %i\n", image_spec->image_height);
-  printf("pixel_depth: %i\n", image_spec->pixel_depth);
-  printf("image_descriptor: %i\n", image_spec->image_descriptor);
-
   // Just don't even support colors.
   assert(header->id_length == 0);
   assert(header->color_map_type == 0);
+  // Get the image_spec. This has overall image details. 
+  TgaImageSpec* image_spec = (TgaImageSpec*)(&buffer[sizeof(TgaHeader)]);
+  // Only support 8-bit pixel depths.
   assert(image_spec->pixel_depth == 8);
 
   // Image bytes sz
   uint32_t image_bytes_size = image_spec->image_width * image_spec->image_height;
+  *image_bytes = (uint8_t*)malloc(image_bytes_size);
+  memcpy(*image_bytes, &buffer[sizeof(TgaHeader) + sizeof(TgaImageSpec)], image_bytes_size);
+  // Free buffer used to read in file.
+  free(buffer);
+  return image_bytes_size;
+}
 
-  uint8_t* pixel_bytes = (uint8_t*)malloc(image_bytes_size);
-  memcpy(pixel_bytes, &buffer[sizeof(TgaHeader) + sizeof(TgaImageSpec)], image_bytes_size);
+int
+main(int argc, char** argv)
+{
+  uint8_t* image_bytes;
+  
+  uint32_t sz = LoadTGA("example/gfx/characters_0.tga", &image_bytes);
+
+  printf("read %i image bytes...\n", sz);
 
   // First pixel is pure black.
-  printf("pixel: %i\n", pixel_bytes[0]);
+  printf("pixel: %i\n", image_bytes[0]);
 
   // These should fade to white
-  printf("pixel: %i\n", pixel_bytes[7]);
-  printf("pixel: %i\n", pixel_bytes[8]);
-  printf("pixel: %i\n", pixel_bytes[8]);
-  printf("pixel: %i\n", pixel_bytes[10]);
-  printf("pixel: %i\n", pixel_bytes[11]);
-  printf("pixel: %i\n", pixel_bytes[12]);
-  printf("pixel: %i\n", pixel_bytes[13]);
-  printf("pixel: %i\n", pixel_bytes[14]);
+  printf("pixel: %i\n", image_bytes[7]);
+  printf("pixel: %i\n", image_bytes[8]);
+  printf("pixel: %i\n", image_bytes[8]);
+  printf("pixel: %i\n", image_bytes[10]);
+  printf("pixel: %i\n", image_bytes[11]);
+  printf("pixel: %i\n", image_bytes[12]);
+  printf("pixel: %i\n", image_bytes[13]);
+  printf("pixel: %i\n", image_bytes[14]);
+
 
   return 0;
 }
