d508be22b87a853aee4c9eced65d70b38491a904
Alan Newton <rufeooo@gmail.com>
2019-12-28 12:20:12 -0800

    first pass at ecs, counter is flawed

---
+++ example/ecs/ecsc.c
@@ -0,0 +1,80 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int x;
+  int y;
+} c1_t;
+
+#define ECS_TYPE(V) \
+  V:                \
+  __COUNTER__
+
+#define TYPE_ID(X) _Generic((X), ECS_TYPE(c1_t), default : -1)
+#define MAX_ECS_TYPE __COUNTER__
+#define PAGE (4 * 1024)
+
+typedef struct {
+  void* memory;
+} ecstype_t;
+ecstype_t typeinfo[MAX_ECS_TYPE];
+
+#define PRINTF_COUNTER(type) printf("%d\n", __COUNTER__)
+
+void
+ecs_init()
+{
+  for (int i = 0; i < MAX_ECS_TYPE; ++i) {
+    typeinfo[i].memory = malloc(PAGE);
+  }
+}
+
+void
+ecs_shutdown()
+{
+  for (int i = 0; i < MAX_ECS_TYPE; ++i) {
+    free(typeinfo[i].memory);
+    typeinfo[i].memory = 0;
+  }
+}
+
+void*
+ecs_memory(int type_id)
+{
+  return typeinfo[type_id].memory;
+}
+
+/*#define ECS_ENUM(ptr_type)         \
+  int f_type = TYPE_ID(*ptr_type); \
+  ptr_type = ecs_memory(f_type);*/
+#define ECS_ENUM(ptr_type) ptr_type = ecs_memory(TYPE_ID(*ptr_type))
+
+int
+main()
+{
+  ecs_init();
+
+  c1_t f;
+  int i;
+  c1_t* iter_f;
+  ECS_ENUM(iter_f);
+  c1_t* iter_end = iter_f + PAGE / sizeof(*iter_f) + 1;
+
+  for (int i = 0; iter_f < iter_end; ++iter_f, ++i) {
+    iter_f->x = i;
+    iter_f->y = i * 10;
+  }
+
+  ECS_ENUM(iter_f);
+  for (int i = 0; iter_f < iter_end; ++iter_f, ++i) {
+    printf("[ %d x ] [ %d y ]\n", iter_f->x, iter_f->y);
+  }
+
+  printf("%d c1\n", TYPE_ID(f));
+  printf("%d int\n", TYPE_ID(i));
+  int arr[] = {[0] = 3};
+  // PRINTF_COUNTER(c1);
+  return 0;
+}
+
