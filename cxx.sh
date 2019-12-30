e39d7baa110e1603638fb361d735f3b850e9c179
Alan Newton <rufeooo@gmail.com>
2019-12-30 08:27:58 -0800

    linux build optimization

--- cxx.sh
+++ cxx.sh
@@ -2,7 +2,13 @@
 source ./run_constants.sh
 
 if [ -n "$unix" ]; then
-  time $CXX $CXXFLAG $1 -I src/ -L bin/ -lX11 -lEGL -lGL -lpthread -o $BIN_DIR/`basename -s .cc $1`
+  # clang++ is 10x slower when pinned to core 0, so use taskset to force core 1
+  CMD="taskset 0x2 $CXX $CXXFLAG $1 -I src/ -lX11 -lEGL -lGL -lpthread -o $BIN_DIR/`basename -s .cc $1`"
+  if [ -n `which perf` ]; then
+    perf stat $CMD
+  else
+    $CMD
+  fi
 else
   $CXX $CXXFLAG $1 -ObjC++ -I src/ -L bin/ -ldl -o $BIN_DIR/`basename -s .cc $1` -framework OpenGL -framework AppKit -mmacosx-version-min=10.7 -stdlib=libc++
   # For teeny weeny builds '-Os -flto'
