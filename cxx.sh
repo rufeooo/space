9a7636fb70828ee2ddcd8102f6c36f08b0545b0c
Alan Newton <rufeooo@gmail.com>
2020-01-03 10:40:30 -0800

    Simplify linux build script

--- cxx.sh
+++ cxx.sh
@@ -2,13 +2,8 @@
 source ./run_constants.sh
 
 if [ -n "$unix" ]; then
-  # clang++ is 10x slower when pinned to core 0, so use taskset to force core 1
-  CMD="taskset 0x2 $CXX $CXXFLAG $1 -I src/ -lX11 -lEGL -lGL -lpthread -o $BIN_DIR/`basename -s .cc $1`"
-  if [ -n `which perf` ]; then
-    perf stat $CMD
-  else
-    $CMD
-  fi
+  echo Building with $CXX
+  time $CXX $CXXFLAG $1 -I src/ -lX11 -lEGL -lGL -lpthread -o $BIN_DIR/`basename -s .cc $1`
 else
   $CXX $CXXFLAG $1 -ObjC++ -I src/ -L bin/ -ldl -o $BIN_DIR/`basename -s .cc $1` -framework OpenGL -framework AppKit -mmacosx-version-min=10.7 -stdlib=libc++
   # For teeny weeny builds '-Os -flto'
