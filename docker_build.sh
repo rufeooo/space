140009e71bdac05e46e28d01d881ed59dee0a13d
Alan Newton <rufeooo@gmail.com>
2020-01-03 17:21:44 -0800

    Docker build overrides CXXFLAGS to strip X11/EGL deps

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-./cxx.sh src/space_server.cc
+# Removes X11/EGL dependencies via linker optimization
+CXXFLAGS="-O1 -std=c++17 -flto" ./cxx.sh src/space_server.cc
 docker build . -t space
