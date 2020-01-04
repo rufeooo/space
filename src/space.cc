ee14c73ba9f1ba94bfa22a6b12eba0e66c5706e6
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2020-01-04 14:09:25 -0800

    Draw a little indicator under the player to show which tile it is on.

--- src/space.cc
+++ src/space.cc
@@ -71,6 +71,7 @@ Initialize()
   {
     auto* transform = &game_entity[0].transform;
     transform->position = math::Vec3f(0.f, 0.f, 0.f);
+    transform->scale = math::Vec3f(0.25f, 0.25f, 0.f);
   }
 
   camera::MoveTo(math::Vec3f(400.f, 400.f, 0.f));
@@ -352,9 +353,20 @@ UpdateGame()
 
   {
     Entity* ent = &game_entity[0];
-    gfx::PushCircle(ent->transform.position, ent->transform.scale,
-                    ent->transform.orientation,
-                    math::Vec4f(1.f, 1.f, 1.f, 1.f));
+    math::Vec3f* p = &ent->transform.position;
+    math::Vec2f grid = {
+      (p->x - fmod(p->x, tilemap::kTileWidth)) + tilemap::kTileWidth / 2.f,
+      (p->y - fmod(p->y, tilemap::kTileHeight)) + tilemap::kTileHeight / 2.f};
+    // Draw the player.
+    gfx::PushRectangle(ent->transform.position, ent->transform.scale,
+                       ent->transform.orientation,
+                       math::Vec4f(0.26f, 0.33f, 0.78f, 1.f));
+
+    // Highlight the tile the player is on.
+    gfx::PushRectangle(math::Vec3f(grid),
+                         math::Vec3f(1.f / 2.f, 1.f / 2.f, 1.f),
+                         math::Quatf(0.f, 0.f, 0.f, 1.f),
+                         math::Vec4f(1.f, 0.f, 0.f, .45f)); 
   }
 
   gfx::PushGrid(50.f, 50.f, math::Vec4f(0.207f, 0.317f, 0.360f, 0.60f));
@@ -365,8 +377,7 @@ UpdateGame()
       using namespace tilemap;
       Tile* tile = &kTilemap.map[i][j];
       if (tile->type == NONE) continue;
-      math::Vec2f world = {tile->x * kTileWidth, tile->y * kTileHeight};
-      gfx::PushRectangle(math::Vec3f(world),
+      gfx::PushRectangle(math::Vec3f(TileToWorld(*tile)),
                          math::Vec3f(1.f / 2.f, 1.f / 2.f, 1.f),
                          math::Quatf(0.f, 0.f, 0.f, 1.f),
                          math::Vec4f(1.f, 1.f, 1.f, 1.f)); 
