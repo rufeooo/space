0f951d3ce7c9c4c1977cbd6230a88ceb748bf661
Alan Newton <rufeooo@gmail.com>
2020-01-10 20:15:09 -0800

    Output windows build result

--- build.bat
+++ build.bat
@@ -4,3 +4,4 @@ set file_arg=%1
 for /F "delims=" %%i in ("%file_arg%") do set filename="%%~ni"
 
 cl %1 /Zi /nologo /std:c++latest /I src\ /Fo:%BIN_DIR%\ /DUNICODE /link user32.lib opengl32.lib gdi32.lib /OUT:%BIN_DIR%/%filename%.exe
+echo result %ERRORLEVEL%
