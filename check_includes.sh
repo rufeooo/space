36ea9912330b23ae097c851301fa91d79cd7a833
Alan Newton <rufeooo@gmail.com>
2020-01-12 12:16:34 -0800

    check_includes.sh: no outputs, just run preprocessor

--- check_includes.sh
+++ check_includes.sh
@@ -22,7 +22,7 @@ for file in ${ARR[@]}; do
 	  echo SKIP $file_basename [platform file]
 	  continue
   fi
-  $CXX $CXXFLAGS -Wno-pragma-once-outside-header -x c++ -c -I src/ $file
+  $CXX $CXXFLAGS -Wno-pragma-once-outside-header -x c++ -E -I src/ $file &>/dev/null
   if [ $? -ne 0 ]; then
 	  echo $file failed
 	  exit 1
