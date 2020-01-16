e0bfc93d347ad907a2c0db723143610600a91ce4
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2020-01-16 06:52:29 -0800

    Squelch the many -Wformat warns from printing uint64_t.

--- cxx.sh
+++ cxx.sh
@@ -5,7 +5,7 @@ if [ -n "$unix" ]; then
   echo Building with $CXX $CXXFLAGS
   time $CXX $CXXFLAGS $1 -I src/ -lX11 -lEGL -lGL -lpthread -o $BIN_DIR/`basename -s .cc $1`
 else
-  time $CXX $CXXFLAGS $1 -g -ObjC++ -I src/ -L bin/ -ldl -o $BIN_DIR/`basename -s .cc $1` -framework OpenGL -framework AppKit -mmacosx-version-min=10.7 -stdlib=libc++
+  time $CXX $CXXFLAGS $1 -g -ObjC++ -I src/ -L bin/ -ldl -o $BIN_DIR/`basename -s .cc $1` -framework OpenGL -framework AppKit -mmacosx-version-min=10.7 -stdlib=libc++ -Wno-format
   # For teeny weeny builds '-Os -flto'
 fi
 
