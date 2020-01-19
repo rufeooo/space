5315a63b4226fddcb70dea6375e53654a299122f
Alan Newton <rufeooo@gmail.com>
2020-01-19 12:33:25 -0800

    Mac/Linux disable fp contraction

--- run_constants.sh
+++ run_constants.sh
@@ -23,7 +23,7 @@ if [ -z "$CC" ]; then
 fi
 
 if [ -z "$CXXFLAGS" ]; then
-  CXXFLAGS="-O1 -fno-omit-frame-pointer -std=c++17 "
+  CXXFLAGS="-O1 -fno-omit-frame-pointer -std=c++17 -ffp-contract=off "
 fi
 
 if [ -n "$DEV_FLAGS" ]; then
