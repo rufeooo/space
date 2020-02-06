a456736be56b7e91eabbdf4c00f84d283b7b0d19
Alan Newton <rufeooo@gmail.com>
2020-02-05 18:35:55 -0800

    Remove process affinity

--- src/platform/platform.cc
+++ src/platform/platform.cc
@@ -14,13 +14,11 @@
 #include "window.cc"
 
 #if _WIN32
-#include "win32_affinity.cc"
 #include "win32_filesystem.cc"
 #include "win32_sleep.cc"
 #include "win32_thread.cc"
 #include "win32_udp.cc"
 #else
-#include "unix_affinity.cc"
 #include "unix_filesystem.cc"
 #include "unix_sleep.cc"
 #include "unix_thread.cc"
