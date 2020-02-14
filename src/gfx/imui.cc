6df9801650d7e6bf62390e01c9eb93bbeaf61d22
Alan Newton <rufeooo@gmail.com>
2020-02-14 05:36:04 -0800

    imui sets an internal errno

--- src/gfx/imui.cc
+++ src/gfx/imui.cc
@@ -5,6 +5,8 @@
 #include "../common/array.cc"
 #include "../common/queue.cc"
 
+EXTERN(unsigned imui_errno);
+
 namespace imui
 {
 constexpr int kMaxTextSize = 128;
@@ -31,8 +33,10 @@ struct PaneOptions {
     kFixedSize,
   };
   PaneOptions() = default;
-  PaneOptions(float width, float height) :
-    size_mode(kFixedSize), width(width), height(height) {}
+  PaneOptions(float width, float height)
+      : size_mode(kFixedSize), width(width), height(height)
+  {
+  }
 
   SizeMode size_mode = kAutoResize;
   float width = 0.f;
@@ -42,7 +46,7 @@ struct PaneOptions {
 struct Text {
   char msg[kMaxTextSize];
   v2f pos;
-  v4f color; // TODO: This is duplicated in TextOptions
+  v4f color;  // TODO: This is duplicated in TextOptions
   math::Rect rect;
   TextOptions options;
 };
@@ -106,8 +110,8 @@ void
 Render()
 {
   auto dims = window::GetWindowSize();
-  rgg::ModifyObserver mod(
-      math::Ortho2(dims.x, 0.0f, dims.y, 0.0f, 0.0f, 0.0f), math::Identity());
+  rgg::ModifyObserver mod(math::Ortho2(dims.x, 0.0f, dims.y, 0.0f, 0.0f, 0.0f),
+                          math::Identity());
 
   for (int i = 0; i < kUsedPane; ++i) {
     Pane* pane = &kPane[i];
@@ -126,9 +130,9 @@ Render()
 
   for (int i = kReadUIClickRender; i < kWriteUIClickRender; ++i) {
     UIClickRender* render = &kUIClickRender[i % kMaxUIClickRender];
-    rgg::RenderCircle(render->pos, 3.5f,
-                      v4f(1.f, 0.f, 0.65f,
-                          (float)render->render_frames / kClickForFrames));
+    rgg::RenderCircle(
+        render->pos, 3.5f,
+        v4f(1.f, 0.f, 0.65f, (float)render->render_frames / kClickForFrames));
     --render->render_frames;
     if (!render->render_frames) {
       PopUIClickRender();
@@ -169,16 +173,18 @@ UpdatePaneOnCall(const math::Rect& rect, Pane* pane)
   // Must call Begin() before UI rendering starts and End() when it's done.
   assert(begin_mode.set);
   switch (pane->options.size_mode) {
-  case PaneOptions::kAutoResize: {
-    begin_mode.pane->rect.y = begin_mode.pos.y;
-    begin_mode.pane->rect.height += rect.height;
-    float width = (rect.x + rect.width) - begin_mode.pane->rect.x;
-    if (width > begin_mode.pane->rect.width) {
-      begin_mode.pane->rect.width = width;
-    }
-  } break;
-  case PaneOptions::kFixedSize: break;
-  default: break;
+    case PaneOptions::kAutoResize: {
+      begin_mode.pane->rect.y = begin_mode.pos.y;
+      begin_mode.pane->rect.height += rect.height;
+      float width = (rect.x + rect.width) - begin_mode.pane->rect.x;
+      if (width > begin_mode.pane->rect.width) {
+        begin_mode.pane->rect.width = width;
+      }
+    } break;
+    case PaneOptions::kFixedSize:
+      break;
+    default:
+      break;
   }
 }
 
@@ -189,25 +195,27 @@ UpdatePaneOnEnd(Pane* pane)
   auto& begin_mode = kIMUI.begin_mode;
   assert(begin_mode.set);
   switch (pane->options.size_mode) {
-  case PaneOptions::kAutoResize: break;
-  case PaneOptions::kFixedSize: {
-    int start = kUsedText - 1;
-    int end = kUsedText - begin_mode.text_calls;
-    assert(start < kMaxText);
-    assert(end >= 0);
-    int move_text = begin_mode.text_calls - 1;
-    for (; start >= end; --start) {
-      struct Text* text = &kText[start];
-      text->pos.y += text->rect.height * move_text;
-      v2f text_top_left = text->pos + v2f(0.f, text->rect.height);
-      // Discard the text element if it is outside of the pane.
-      if (!math::PointInRect(text_top_left, pane->rect)) {
-        CompressText(start);
-        continue;
+    case PaneOptions::kAutoResize:
+      break;
+    case PaneOptions::kFixedSize: {
+      int start = kUsedText - 1;
+      int end = kUsedText - begin_mode.text_calls;
+      assert(start < kMaxText);
+      assert(end >= 0);
+      int move_text = begin_mode.text_calls - 1;
+      for (; start >= end; --start) {
+        struct Text* text = &kText[start];
+        text->pos.y += text->rect.height * move_text;
+        v2f text_top_left = text->pos + v2f(0.f, text->rect.height);
+        // Discard the text element if it is outside of the pane.
+        if (!math::PointInRect(text_top_left, pane->rect)) {
+          CompressText(start);
+          continue;
+        }
       }
-    }
-  } break;
-  default: break;
+    } break;
+    default:
+      break;
   }
 }
 
@@ -217,11 +225,11 @@ Text(const char* msg, v2f pos, TextOptions options)
   Result data;
   struct Text* text = UseText();
   if (!text) {
-    printf("imui text count exhausted.\n");
+    imui_errno = 1;
     return data;
   }
   if (strlen(msg) > kMaxTextSize) {
-    printf("text provided surpasses max allowed imui character count.\n");
+    imui_errno = 2;
     return data;
   }
   auto& begin_mode = kIMUI.begin_mode;
@@ -234,7 +242,7 @@ Text(const char* msg, v2f pos, TextOptions options)
   if (data.highlighted && options.highlight_color != v4f()) {
     text->color = options.highlight_color;
   }
-  text->options = options; 
+  text->options = options;
   text->rect = data.rect;
   ++begin_mode.text_calls;
   return data;
@@ -275,7 +283,7 @@ Begin(v2f start, const PaneOptions& pane_options)
   // End must be called before Begin.
   assert(!begin_mode.set);
   begin_mode.pos = start;
-  begin_mode.set = true; 
+  begin_mode.set = true;
   begin_mode.text_calls = 0;
   begin_mode.pane = UsePane();
   begin_mode.pane->rect.x = start.x;
@@ -292,7 +300,7 @@ Begin(v2f start)
   // End must be called before Begin.
   assert(!begin_mode.set);
   begin_mode.pos = start;
-  begin_mode.set = true; 
+  begin_mode.set = true;
   begin_mode.pane = nullptr;
 }
 
@@ -300,7 +308,7 @@ void
 End()
 {
   UpdatePaneOnEnd(kIMUI.begin_mode.pane);
-  kIMUI.begin_mode.set = false; 
+  kIMUI.begin_mode.set = false;
 }
 
 Result
@@ -309,7 +317,7 @@ Button(const math::Rect& rect, const v4f& color)
   struct Button* button = UseButton();
   Result result;
   if (!button) {
-    printf("imui button count exhausted.\n");
+    imui_errno = 3;
     return result;
   }
   button->rect = rect;
@@ -325,10 +333,30 @@ MouseClick(v2f pos)
 {
   UIClick* click = UseUIClick();
   if (!click) {
-    printf("imui click count exhausted.\n");
+    imui_errno = 4;
     return;
   }
   click->pos = pos;
   PushUIClickRender({pos, kClickForFrames});
 }
+
+const char*
+LastErrorString()
+{
+  unsigned err = imui_errno;
+  imui_errno = 0;
+  switch (err) {
+    case 1:
+      return ("imui text count exhausted.");
+    case 2:
+      return ("text provided surpasses max allowed imui character count.");
+    case 3:
+      return ("imui button count exhausted.");
+    case 4:
+      return ("imui click count exhausted.");
+  };
+
+  return 0;
 }
+
+}  // namespace imui
