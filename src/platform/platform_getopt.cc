c7aa0f32aba9ca98cdfa93dd698a887b80af80e5
Alan Newton <rufeooo@gmail.com>
2020-02-25 18:26:22 -0800

    Fix a crash in platform_getopt

--- src/platform/platform_getopt.cc
+++ src/platform/platform_getopt.cc
@@ -1,6 +1,8 @@
 
 #include "macro.h"
 
+static const char* kEmptyString = "";
+
 EXTERN(int platform_optind = 1);
 EXTERN(const char* platform_optarg);
 
@@ -9,13 +11,14 @@ platform_getopt(int argc, char* const argv[], const char* optstring)
 {
   for (; platform_optind < argc; ++platform_optind) {
     const char* arg = argv[platform_optind];
-    platform_optarg = argv[platform_optind + 1];
+    platform_optarg = kEmptyString;
 
     const char* optiter = optstring;
     for (; *optiter; ++optiter) {
       int val_param = *(optiter + 1) == ':';
       if (arg[0] == '-' && *optiter == arg[1]) {
-        if (!val_param) platform_optarg = nullptr;
+        if (val_param && platform_optind + 1 < argc)
+          platform_optarg = argv[platform_optind + 1];
         ++platform_optind;
         return *optiter;
       }
