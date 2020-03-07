90f943340b94cd3858068cbe9a24a1c018367914
Alan Newton <rufeooo@gmail.com>
2020-03-07 11:03:03 -0800

    Server now buffers packets

--- src/network/server.cc
+++ src/network/server.cc
@@ -12,10 +12,17 @@ struct ServerParam {
 };
 static ServerParam thread_param;
 
+#define MAX_GAMEQUEUE 128
+#define GAMEQUEUE_SLOT(sequence) ((sequence) % MAX_GAMEQUEUE)
+#define MAX_GAME 10
+#define MAX_PLAYER 2
+#define MAX_PACKET 1024
+#define TIMEOUT_USEC (2 * 1000 * 1000)
+
 struct PlayerState {
   Udp4 peer;
   uint64_t num_players;
-  uint64_t game_id;
+  uint64_t game_index = UINT64_MAX;
   uint64_t pending_game_id;
   uint64_t last_active;
   uint64_t sequence;
@@ -24,15 +31,24 @@ struct PlayerState {
   uint64_t cookie_mismatch;
 };
 static PlayerState zero_player;
+static PlayerState player[MAX_PLAYER];
+
+struct Game {
+  // Unique id of a game session or 0 when unused
+  uint64_t game_id;
+  // Players in game at launch
+  uint64_t num_players;
+  // Current simulation frame of the game session
+  uint64_t frame;
+  // Game data (no protocol wrapper)
+  uint8_t slot[MAX_GAMEQUEUE][MAX_PLAYER][MAX_PACKET];
+  // Game byte count
+  uint64_t used_slot[MAX_GAMEQUEUE][MAX_PLAYER];
+};
+static Game game[MAX_GAME];
 
 static bool running = true;
-#define MAX_BUFFER (4 * 1024)
-#define MAX_PLAYER 2
-PlayerState player[MAX_PLAYER];
-uint64_t next_game_id = time(0);
-bool game_ready;
-Clock_t server_clock;
-#define TIMEOUT_USEC (2 * 1000 * 1000)
+static uint64_t next_game_id = time(0);
 
 int
 GetPlayerIndexFromPeer(Udp4* peer)
@@ -54,19 +70,112 @@ GetNextPlayerIndex()
   return -1;
 }
 
+int
+GetGameIndex(uint64_t game_id)
+{
+  for (int i = 0; i < MAX_GAME; ++i) {
+    if (game[i].game_id == game_id) return i;
+  }
+  for (int i = 0; i < MAX_GAME; ++i) {
+    if (game[i].game_id == 0) return i;
+  }
+
+  return -1;
+}
+
+bool
+PlayerInGame(uint64_t gidx)
+{
+  for (int i = 0; i < MAX_PLAYER; ++i) {
+    if (player[i].game_index == gidx) return true;
+  }
+  return false;
+}
+
 void
 prune_players(uint64_t rt_usec)
 {
   for (int i = 0; i < MAX_PLAYER; ++i) {
     if (memcmp(&zero_player, &player[i], sizeof(PlayerState)) == 0) continue;
     if (rt_usec - player[i].last_active > TIMEOUT_USEC) {
+      printf("Server dropped packet flow. [ index %d ] [ game_index %lu ]\n", i,
+             player[i].game_index);
       player[i] = PlayerState{};
-      printf("Server dropped packet flow. [index %d]\n", i);
     }
     if (player[i].cookie_mismatch > 3) {
-      player[i] = PlayerState{};
       printf("Server closed packet flow: cookie_mismatch. [index %d]\n", i);
+      player[i] = PlayerState{};
     }
+  }
+}
+
+void
+prune_games()
+{
+  bool active_game[MAX_GAME] = {};
+
+  for (int pidx = 0; pidx < MAX_PLAYER; ++pidx) {
+    uint64_t game_index = player[pidx].game_index;
+    if (game_index == UINT64_MAX) continue;
+    active_game[game_index] = 1;
+  }
+
+  for (int gidx = 0; gidx < MAX_GAME; ++gidx) {
+    if (active_game[gidx]) continue;
+    uint64_t game_id = game[gidx].game_id;
+    if (!game_id) continue;
+    printf("Server removed game [ game_index %d ] [ game_id %lu ]\n", gidx,
+           game_id);
+    game[gidx] = {};
+  }
+}
+
+void
+update_game(Udp4 location, uint64_t game_index)
+{
+  static uint8_t out_buffer[MAX_PACKET];
+  Game* g = &game[game_index];
+  const uint64_t game_id = g->game_id;
+  const uint64_t next_frame = g->frame + 1;
+  if (!game_id) return;
+
+#if 0
+  printf("Update game [ game_id %lu ] [ num_players %lu ]\n", g->game_id,
+         g->num_players);
+#endif
+  uint64_t sidx = GAMEQUEUE_SLOT(next_frame);
+  for (uint64_t i = 0; i < g->num_players; ++i) {
+    if (g->used_slot[sidx][i] == 0) return;
+  }
+  for (int pidx = 0; pidx < MAX_PLAYER; ++pidx) {
+    if (player[pidx].game_index != game_index) continue;
+
+    // Send player packets for each participant's frame
+    uint64_t num_players = player[pidx].num_players;
+    uint64_t ack = player[pidx].sequence;
+    for (int j = 0; j < num_players; ++j) {
+      NotifyTurn* nt = (NotifyTurn*)out_buffer;
+      nt->frame = next_frame;
+      nt->player_id = j;
+      nt->ack_sequence = ack;
+      uint64_t event_bytes = g->used_slot[sidx][j];
+      memcpy(nt->event, g->slot[sidx][j], event_bytes);
+      if (!udp::SendTo(location, player[pidx].peer, out_buffer,
+                       event_bytes + sizeof(NotifyTurn))) {
+        puts("server send failed");
+        break;
+      }
+    }
+  }
+
+#if 0
+  printf("Server send [ frame %lu ]\n", next_frame);
+#endif
+  g->frame = next_frame;
+  // TODO (AN): Add client ack_frame to protocol to allow retransmit
+  //   This code assumes no packet loss and clears after one send
+  for (uint64_t i = 0; i < g->num_players; ++i) {
+    g->used_slot[sidx][i] = 0;
   }
 }
 
@@ -82,7 +191,7 @@ server_main(void* void_arg)
            platform::thread_affinity_count());
   }
 
-  uint8_t in_buffer[MAX_BUFFER];
+  uint8_t in_buffer[MAX_PACKET];
   if (!udp::Init()) {
     puts("server: fail init");
     return 1;
@@ -104,6 +213,7 @@ server_main(void* void_arg)
 
   uint64_t realtime_usec = 0;
   uint64_t time_step_usec = 1000;
+  Clock_t server_clock;
   platform::clock_init(time_step_usec, &server_clock);
   while (running) {
     uint16_t received_bytes;
@@ -113,8 +223,12 @@ server_main(void* void_arg)
     if (platform::clock_sync(&server_clock, &sleep_usec)) {
       realtime_usec += time_step_usec;
       prune_players(realtime_usec);
+      prune_games();
+      for (int i = 0; i < MAX_GAME; ++i) {
+        update_game(location, i);
+      }
     }
-    if (!udp::ReceiveAny(location, MAX_BUFFER, in_buffer, &received_bytes,
+    if (!udp::ReceiveAny(location, MAX_PACKET, in_buffer, &received_bytes,
                          &peer)) {
       if (udp_errno) running = false;
       if (udp_errno) printf("Server udp_errno %d\n", udp_errno);
@@ -138,7 +252,7 @@ server_main(void* void_arg)
       printf("Server Accepted Handshake [index %d]\n", player_index);
       player[player_index].peer = peer;
       player[player_index].num_players = num_players;
-      player[player_index].game_id = 0;
+      player[player_index].game_index = UINT64_MAX;
       player[player_index].pending_game_id = 0;
       player[player_index].last_active = realtime_usec;
       player[player_index].sequence = 0;
@@ -169,7 +283,7 @@ server_main(void* void_arg)
               i, player_id, num_players, next_game_id, player_cookie);
           response->player_id = player_id;
           response->player_count = num_players;
-          response->game_id = next_game_id - 1;
+          response->game_id = next_game_id;
           response->cookie = player_cookie;
           udp::SendTo(location, player[i].peer, in_buffer, sizeof(NotifyGame));
           player[i].pending_game_id = next_game_id;
@@ -177,61 +291,78 @@ server_main(void* void_arg)
           player[i].cookie = player_cookie;
           ++player_id;
         }
-        ++next_game_id;
+        next_game_id += 1 + (next_game_id == 0);
       }
     }
 
     // Filter Identified clients
-    if (pidx == -1) continue;
+    if (pidx == -1) {
+      puts("unknown client && packet");
+      continue;
+    }
 
     // Mark player connection active
     player[pidx].last_active = realtime_usec;
 
-    // Filter for game-ready clients
-    if (!player[pidx].game_id) {
+    // Handle pending games
+    uint64_t gidx = player[pidx].game_index;
+    if (gidx == UINT64_MAX) {
       if (received_bytes == sizeof(BeginGame)) {
         BeginGame* bgPacket = (BeginGame*)(in_buffer);
         if (player[pidx].cookie != bgPacket->cookie) {
           player[pidx].cookie_mismatch += 1;
+          puts("cookie mismatch");
           continue;
         }
         // two-way interest is agreed, copy pending_game_id to game_id
-        player[pidx].game_id = player[pidx].pending_game_id;
+        uint64_t game_id = player[pidx].pending_game_id;
+        gidx = GetGameIndex(game_id);
+        if (gidx == -1) {
+          puts("Server is out of space for Space");
+          continue;
+        }
+        player[pidx].game_index = gidx;
+        game[gidx].game_id = game_id;
+        game[gidx].num_players = player[pidx].num_players;
+        game[gidx].frame = 0;
+        printf("Server created Game [ game_index %lu ]\n", gidx);
+        continue;
       }
 
+#if 0
+      puts("Unknown message during pending state");
+#endif
       continue;
     }
 
-    Turn* packet = (Turn*)in_buffer;
-    uint64_t game_id = player[pidx].game_id;
-#if 0
-    printf(
-        "SvrRcv [ %d socket ] [ %d bytes ] [ %lu sequence ] [ %lu game_id ]\n",
-        location.socket, received_bytes, packet->sequence, game_id);
-#endif
     // Require stream integrity
-    if (packet->sequence - player[pidx].sequence != 1) continue;
+    Turn* packet = (Turn*)in_buffer;
+    int64_t player_delta = packet->sequence - player[pidx].sequence;
+    if (player_delta >= MAX_GAMEQUEUE || player_delta <= 0) {
+      // puts("packet sequence not relevant to player");
+      continue;
+    }
     player[pidx].sequence = packet->sequence;
 
-    // NotifyTurn
-    uint64_t event_bytes = received_bytes - sizeof(Turn);
-    uint8_t out_buffer[MAX_BUFFER];
-    NotifyTurn* nt = (NotifyTurn*)out_buffer;
-    nt->frame = packet->sequence;
-    nt->player_id = player[pidx].player_id;
-    nt->ack_sequence = packet->sequence;
-    memcpy(nt->event, packet->event, event_bytes);
-
-    // Echo bytes to game participants
-    for (int i = 0; i < MAX_PLAYER; ++i) {
-      if (player[i].game_id != game_id) continue;
-
-      if (!udp::SendTo(location, player[i].peer, out_buffer,
-                       event_bytes + sizeof(NotifyTurn))) {
-        puts("server send failed");
-        break;
-      }
+    // Verify relevance to game state
+    int64_t game_delta = packet->sequence - game[gidx].frame;
+    if (game_delta >= MAX_GAMEQUEUE || game_delta <= 0) {
+      // puts("packet seqeuence not relevant to game");
+      continue;
     }
+
+    // Handle storage of new packet in game
+    uint64_t event_bytes = received_bytes - sizeof(Turn);
+    uint64_t game_id = game[gidx].game_id;
+    uint64_t sidx = GAMEQUEUE_SLOT(packet->sequence);
+    uint64_t pid = player[pidx].player_id;
+    memcpy(game[gidx].slot[sidx][pid], packet->event, event_bytes);
+    game[gidx].used_slot[sidx][pid] = event_bytes;
+#if 1
+    printf(
+        "SvrRcv [ %d socket ] [ %d bytes ] [ %lu sequence ] [ %lu game_id ] \n",
+        location.socket, received_bytes, packet->sequence, game_id);
+#endif
   }
 
   return 0;
