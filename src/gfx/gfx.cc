621a16dbcf0b9051138fc8d35d5d732458962ab0
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2020-03-21 12:12:22 -0700

    Resolution fix...

--- src/gfx/gfx.cc
+++ src/gfx/gfx.cc
@@ -29,7 +29,7 @@ static v3f kTileScale = v3f(0.5f, 0.5f, 1.f);
 bool
 Initialize()
 {
-  int window_result = window::Create("Space", 3440, 1440, true);
+  int window_result = window::Create("Space", 1980, 1020, false);
   printf("Window create result: %i\n", window_result);
   auto status = rgg::Initialize();
   constexpr int kVertCount = 29;
