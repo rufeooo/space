84d1bf4491c1bb94e1594634f7b3147cb8c50f9e
Alan Newton <rufeooo@gmail.com>
2020-03-21 21:32:38 -0700

    Enable fullscreen

--- src/gfx/gfx.cc
+++ src/gfx/gfx.cc
@@ -29,7 +29,7 @@ static v3f kTileScale = v3f(0.5f, 0.5f, 1.f);
 bool
 Initialize()
 {
-  int window_result = window::Create("Space", 1920, 1080, false);
+  int window_result = window::Create("Space", 1920, 1080, true);
   printf("Window create result: %i\n", window_result);
   auto status = rgg::Initialize();
   constexpr int kVertCount = 29;
