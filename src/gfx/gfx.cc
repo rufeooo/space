e7d8693ebde9340d8ea7377190ede8c761828f30
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2020-03-23 15:55:43 -0700

    Multi tilemap is hard.

--- src/gfx/gfx.cc
+++ src/gfx/gfx.cc
@@ -329,7 +329,7 @@ RenderShip(uint64_t ship_index)
 
   for (int i = 0; i < kUsedPlayer; ++i) {
     Player* p = &kPlayer[i];
-    TilemapModify mod(p->ship_index);
+    TilemapModify mod(TilemapWorldToGrid(p->world_mouse));
     v2i mouse_grid;
     if (!WorldToTilePos(p->world_mouse, &mouse_grid)) continue;
 
