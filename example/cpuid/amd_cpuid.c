c47546e3014a210e160b49a89b04e96a654103eb
Alan Newton <rufeooo@gmail.com>
2020-03-28 11:58:53 -0700

    Read amd pstates

--- example/cpuid/amd_cpuid.c
+++ example/cpuid/amd_cpuid.c
@@ -1,36 +1,135 @@
+#include <stdio.h>
 
-#define MAX_HW_PSTATES 10
-#define MSR_AMD_PSTATE_LIMIT  0xc0010061
+#define MAX_HW_PSTATE 10
+#define MSR_AMD_PSTATE 0xc0010064
+#define MSR_AMD_HWCR 0xc0010015
 
-int read_msr(unsigned int idx, unsigned long long *val)
+int
+read_msr(unsigned int idx, unsigned long long* val)
 {
-  int fd;
+  FILE* fd;
   char msr_file_name[64];
 
   const int cpu = 0;
   sprintf(msr_file_name, "/dev/cpu/%d/msr", cpu);
-  fd = open(msr_file_name, O_RDONLY);
-  if (fd < 0)
+  fd = fopen(msr_file_name, "r");
+  if (fd < 0) {
+    perror("fopen");
     return -1;
-  if (lseek(fd, idx, SEEK_CUR) == -1)
+  }
+  if (fseek(fd, idx, SEEK_CUR) == -1) {
+    perror("seek");
     goto err;
-  if (read(fd, val, sizeof *val) != sizeof *val)
+  }
+  if (fread(val, sizeof(*val), 1, fd) != 1) {
+    perror("read");
     goto err;
-  close(fd);
+  }
+  fclose(fd);
   return 0;
- err:
-  close(fd);
+err:
+  fclose(fd);
   return -1;
+}
+
+union msr_pstate {
+  struct {
+    unsigned fid : 6;
+    unsigned did : 3;
+    unsigned vid : 7;
+    unsigned res1 : 6;
+    unsigned nbdid : 1;
+    unsigned res2 : 2;
+    unsigned nbvid : 7;
+    unsigned iddval : 8;
+    unsigned idddiv : 2;
+    unsigned res3 : 21;
+    unsigned en : 1;
+  } bits;
+  struct {
+    unsigned fid : 8;
+    unsigned did : 6;
+    unsigned vid : 8;
+    unsigned iddval : 8;
+    unsigned idddiv : 2;
+    unsigned res1 : 31;
+    unsigned en : 1;
+  } fam17h_bits;
+  unsigned long long val;
+};
+
+static int
+get_did(int family, union msr_pstate pstate)
+{
+  int t;
+
+  if (family == 0x12)
+    t = pstate.val & 0xf;
+  else if (family == 0x17 || family == 0x18)
+    t = pstate.fam17h_bits.did;
+  else
+    t = pstate.bits.did;
+
+  return t;
+}
+
+static int
+get_cof(int family, union msr_pstate pstate)
+{
+  int t;
+  int fid, did, cof;
+
+  did = get_did(family, pstate);
+  if (family == 0x17 || family == 0x18) {
+    fid = pstate.fam17h_bits.fid;
+    cof = 200 * fid / did;
+  } else {
+    t = 0x10;
+    fid = pstate.bits.fid;
+    if (family == 0x11) t = 0x8;
+    cof = (100 * (fid + t)) >> did;
+  }
+  return cof;
 }
 
 void
 amd_main(cpu_t cpu)
 {
-  if (cpu.family < 0x10 || cpu.family == 0x14) return;
+  int boost_count = 0;
 
-  unsigned long long val;
-  if (read_msr(cpu, MSR_AMD_PSTATE_LIMIT, &val))
-    return -1;
+  if (cpu.family < 0x10 || cpu.family == 0x14) {
+    puts("AMD family should read ACPI tables. Not supported ATM.");
+    return;
+  }
+  // TODO: filter boost states
+  if (cpu.amd_boost) {
+    puts("AMD family with boost states not yet supported");
+    return;
+    /* TODO: The first N entries are boost states and not relevant to tsc
+     * if (cpu.family == 0x17 || cpu.family == 0x18) {
+      if (read_msr(cpu, MSR_AMD_HWCR, &val) == 0) {
+        boost_count = !(val & CPUPOWER_AMD_CPBDIS);
+      }
+    } else {
+      boost_count = amd_pci_get_num_boost_states(active, states);
+    }*/
+  }
 
-  printf("%ull msr\n", val);
+  int pstates[MAX_HW_PSTATE];
+  int pstate_count = 0;
+  for (int i = 0; i < MAX_HW_PSTATE; ++i) {
+    union msr_pstate pstate;
+    printf("Reading %d\n", i);
+    if (read_msr(MSR_AMD_PSTATE + i, &pstate.val)) break;
+    if ((cpu.family == 0x17) && (!pstate.fam17h_bits.en))
+      continue;
+    else if (!pstate.bits.en)
+      continue;
+
+    pstates[pstate_count++] = get_cof(cpu.family, pstate);
+  }
+
+  for (int i = 0; i < pstate_count; ++i) {
+    printf("%d MHz\n", pstates[i]);
+  }
 }
