57eba4d298acfeb5fa3c60c2b2626b1e1ffe3a29
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2020-04-07 07:09:33 -0700

    Win32 fix due to - https://docs.microsoft.com/en-us/cpp/error-messages/compiler-errors-1/compiler-error-c2397?view=vs-2019

--- src/common/constants.h
+++ src/common/constants.h
@@ -6,10 +6,9 @@
 
 // Invalid offset into an array type 
 // Equivalent to memset(0xff) for up to 64 bits
-// MAY be converted to unsigned types of 64 bits or less, resulting in truncation (i.e. UINT##_MAX)
-constexpr int64_t kInvalidIndex = -1;
+constexpr uint64_t kInvalidIndex = -1;
 
 // Invalid unique identifier ('id') for assigning stable identification numbers
 // Equivalent to memset(0x00) for up to 64 bits
 // Allows for boolean tests: (id ? "exists: "not-exists")
-constexpr int64_t kInvalidId = 0;
+constexpr uint64_t kInvalidId = 0;
