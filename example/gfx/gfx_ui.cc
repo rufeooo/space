69635a85292f7a7bb676ca8606dbaf80ecdee75a
AnthonyBrunasso <AnthonyBrunasso@gmail.com>
2020-04-12 09:21:47 -0700

    Update gfx_ui.cc

--- example/gfx/gfx_ui.cc
+++ example/gfx/gfx_ui.cc
@@ -40,7 +40,7 @@ TextTest()
 
   {
     imui::PaneOptions pane_options;
-    imui::Begin(v2f(dims.x / 2.f - 400.0f, dims.y / 2.f + 160.0f), pane_options);
+    imui::Begin(v2f(dims.x / 2.f - 400.0f, dims.y / 2.f + 160.0f), 0, pane_options);
     imui::TextOptions text_options;
     text_options.highlight_color = v4f(1.0f, 0.0f, 0.0f, 1.0f);
     text_options.color = gfx::kWhite;
@@ -59,19 +59,16 @@ TextTest()
     imui::End();
   }
 
-  {
-    imui::PaneOptions pane_options(300.0f, 100.0f);
-    imui::TextOptions text_options;
-    text_options.scale = 0.75f;
-    imui::Begin(v2f(0.0f, 0.0f), pane_options);
-    imui::End();
-  }
 }
 
 int
 main(int argc, char** argv)
 {
-  gfx::Initialize();
+  window::CreateInfo create_info;
+  create_info.window_width = 1920;
+  create_info.window_height = 1080;
+  gfx::Initialize(create_info);
+
 
   while (!window::ShouldClose()) {
     PlatformEvent event;
@@ -79,18 +76,21 @@ main(int argc, char** argv)
       switch (event.type) {
         case MOUSE_DOWN: {
           if (event.button == BUTTON_LEFT) {
-            imui::MouseClick(event.position, event.button);
+            imui::MouseClick(event.position, event.button, 0);
           }
         } break;
         default: break;
       }
     }
 
+    const v2f cursor = window::GetCursorPosition();
+    imui::MousePosition(cursor, 0);
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     TextTest();
-    imui::Render();
-    imui::Reset();
+    imui::Render(0);
+    imui::ResetAll();
     window::SwapBuffers();
     platform::sleep_usec(10*1000);
   }
