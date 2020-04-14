faf8fc7ab527614d1dc75d6359a318b593ecfe6a
Alan Newton <rufeooo@gmail.com>
2020-04-14 04:47:56 -0700

    Fix fleet bounding box

--- src/simulation/ship.cc
+++ src/simulation/ship.cc
@@ -21,6 +21,25 @@ ShipBounds(uint64_t ship_index)
 }
 
 Rectf
+FleetBounds()
+{
+  // Find min and max bounds of all grids.
+  v2f min(FLT_MAX, FLT_MAX);
+  v2f max(0.0f, 0.0f);
+  for (int i = 0; i < kUsedShip; ++i) {
+    Rectf bounds = ShipBounds(i);
+
+    min.x = fmin(bounds.x, min.x);
+    min.y = fmin(bounds.y, min.y);
+
+    max.x = fmax(bounds.x + bounds.width, max.x);
+    max.y = fmax(bounds.y + bounds.height, max.y);
+  }
+
+  return Rectf(min.x, min.y, max.x - min.x, max.y - min.y);
+}
+
+Rectf
 FromShip(Tile tile)
 {
   Rectf tile_bounds = {};
@@ -29,8 +48,8 @@ FromShip(Tile tile)
   assert(ship_index < kUsedShip);
 
   v3f ship_position = kShip[ship_index].transform.position;
-  tile_bounds.x = ship_position.x + (tile.cx*kTileWidth);
-  tile_bounds.y = ship_position.y + (tile.cy*kTileHeight);
+  tile_bounds.x = ship_position.x + (tile.cx * kTileWidth);
+  tile_bounds.y = ship_position.y + (tile.cy * kTileHeight);
   tile_bounds.width = kTileWidth;
   tile_bounds.height = kTileHeight;
 
@@ -49,7 +68,7 @@ ToShip(uint64_t ship_index, v3f world)
   v2f relpos = world.xy() - kShip[ship_index].transform.position.xy();
   v2i grid(relpos.x / kTileWidth, relpos.y / kTileHeight);
   tile = kCurrentGrid->tilemap[grid.y][grid.x];
-  return tile; 
+  return tile;
 }
 
 Tile
@@ -59,27 +78,8 @@ ToAnyShip(v3f world)
     Tile tile = ToShip(i, world);
     if (TileValid(tile)) return tile;
   }
-  
+
   return kZeroTile;
-}
-
-Rectf
-FleetBounds()
-{
-  // Find min and max bounds of all grids.
-  v2f min(FLT_MAX, FLT_MAX);
-  v2f max_dims(0.0f, 0.0f);
-  for (int i = 0; i < kUsedShip; ++i) {
-    Rectf bounds = ShipBounds(i);
-
-    min.x = fmin(bounds.x, min.x);
-    min.y = fmin(bounds.y, min.y);
-
-    max_dims.x = fmax(bounds.width, max_dims.x);
-    max_dims.y = fmax(bounds.height, max_dims.y);
-  }
-
-  return Rectf(min.x, min.y, max_dims.x, max_dims.y);
 }
 
 }  // namespace simulation
